7fa3ff4cc2c624a7119af0ece1be5176a95f41af
alex <766f6964+github@gmail.com>
2026-04-23 12:00:02 +0200

    task/project models changed

--- golang-backend/internal/models/project.go
+++ golang-backend/internal/models/project.go
@@ -1,6 +1,15 @@
 package models
 
 type Project struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID          int    `json:"id"`
+	Key         string `json:"key"`
+	Summary     string `json:"summary"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+	CreatedBy   User   `json:"createdBy"`
+	CreatedAt   int64  `json:"createdAt"`
+	FinishedAt  int64  `json:"finishedAt"`
+	StartDate   int64  `json:"startDate"`
+	DueDate     int64  `json:"dueDate"`
+	// TODO: type/category, tags, lead, asignee, priority, status
 }
--- golang-backend/internal/models/task.go
+++ golang-backend/internal/models/task.go
@@ -1,6 +1,6 @@
 package models
 
 type Task struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID      int    `json:"id"`
+	Summary string `json:"summary"`
 }
