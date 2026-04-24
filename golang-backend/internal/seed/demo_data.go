2ae925cb97c5f140ce10456d68cd151f2014b3a8
alex <766f6964+github@gmail.com>
2026-04-24 09:30:22 +0200

    new seed package for inserting demo data

---
+++ golang-backend/internal/seed/demo_data.go
@@ -0,0 +1,28 @@
+package seed
+
+import "database/sql"
+
+func CreateDemoData(db *sql.DB) {
+	// TODO
+	/*
+		projectRepository := repositories.NewProjectRepository(db)
+		projectService := services.NewProjectService(projectRepository)
+		err := projectService.AddProject(context.Background(), models.Project{
+			ID:             func() string { u, _ := uuid.NewV7(); return u.String() }(),
+			Key:            "AA",
+			Summary:        "Summary",
+			Description:    nil,
+			CreatedBy:      models.UserBase{ID: "019dba5d-83a4-7f97-bdf1-97a5fb3d5869"},
+			CreatedAt:      utils.CurrentTimestamp(),
+			LastModifiedAt: nil,
+			StartedAt:      nil,
+			FinishedAt:     nil,
+			DueAt:          nil,
+			Type:           models.ProjectType{ID: "019dba85-0669-7fd4-86ed-dbe36df285af"},
+		})
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	*/
+
+}
