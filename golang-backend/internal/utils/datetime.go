a7c629803217c2d5e41e3fa280e3df44de52fc1a
alex <766f6964+github@gmail.com>
2026-04-24 22:07:07 +0200

    new random timestamp func

--- golang-backend/internal/utils/datetime.go
+++ golang-backend/internal/utils/datetime.go
@@ -1,7 +1,26 @@
 package utils
 
-import "time"
+import (
+	"math/rand"
+	"time"
+)
 
 func CurrentMSTimestamp() int64 {
 	return time.Now().UnixNano() / int64(time.Millisecond)
+}
+
+func GetRandomMSTimestamp(start, end time.Time) int64 {
+	if start.After(end) {
+		start, end = end, start
+	}
+	startTimestamp := start.UnixNano() / int64(time.Millisecond)
+	endTimestamp := end.UnixNano() / int64(time.Millisecond)
+
+	diff := endTimestamp - startTimestamp
+
+	randomOffset := rand.Int63n(diff)
+
+	randomTimestamp := startTimestamp + randomOffset
+
+	return randomTimestamp
 }
