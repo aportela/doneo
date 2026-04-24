0bd154512e6bfabed3493297f1651b24e4919d86
alex <766f6964+github@gmail.com>
2026-04-24 11:58:21 +0200

    configuration creation fixed

--- golang-backend/internal/configuration/configuration.go
+++ golang-backend/internal/configuration/configuration.go
@@ -1,6 +1,7 @@
 package configuration
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -9,14 +10,42 @@ import (
 	"github.com/spf13/viper"
 )
 
+const configurationFilename = "configuration"
+const configurationType = "yaml"
+
+func getDataPath() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error getting data directory:", err)
+	}
+	return filepath.Join(pwd, "data")
+}
+
+func createDataPathIfRequired() error {
+	if err := os.MkdirAll(getDataPath(), os.ModePerm); err != nil {
+		return err
+	}
+	return nil
+}
+
+func initViper() {
+	viper.AddConfigPath(getDataPath())
+	viper.SetConfigName(configurationFilename)
+	viper.SetConfigType(configurationType)
+}
+
 func Init() {
 	// TODO: return error (replace log.Fatal)
-	viper.AddConfigPath(filepath.Join(".", "data"))
-	viper.SetConfigName("gotask.config")
-	viper.SetConfigType("yaml")
-	err := viper.ReadInConfig()
+	err := createDataPathIfRequired()
 	if err != nil {
-		if os.IsNotExist(err) {
+		log.Fatal("Error checking data path:", err)
+	}
+	initViper()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		var notFoundErr viper.ConfigFileNotFoundError
+		if errors.As(err, &notFoundErr) {
 			log.Println("Configuration file not found, creating new with default values")
 			viper.SetDefault("database.path", "test")
 			err := writeDefaultConfig()
@@ -32,15 +61,9 @@ func Init() {
 }
 
 func writeDefaultConfig() error {
-	databaseDir := filepath.Join(".", "data")
-	if err := os.MkdirAll(databaseDir, os.ModePerm); err != nil {
-		return err
-	}
+	viper.Set("database.path", filepath.Join(getDataPath(), "gotask.sqlite3"))
 
-	viper.Set("database.path", "/data/gotask.sqlite3")
-
-	configFile := filepath.Join(databaseDir, "gotask.config.yaml")
-	err := viper.WriteConfigAs(configFile)
+	err := viper.SafeWriteConfig()
 	if err != nil {
 		return err
 	}
