ff9f8150bb8416807699c2a9b39694de9505d029
alex <766f6964+github@gmail.com>
2026-04-25 21:59:03 +0200

    removed unnecesary directive

--- vite-vue-ts-frontend/vite.config.ts
+++ vite-vue-ts-frontend/vite.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
       "/api": {
         target: "http://127.0.0.1:8086",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
