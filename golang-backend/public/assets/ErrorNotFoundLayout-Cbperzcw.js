6de403823e488905865814d71b2a146332675a30
alex <766f6964+github@gmail.com>
2026-04-25 19:03:56 +0200

    added new test build

---
+++ golang-backend/public/assets/ErrorNotFoundLayout-Cbperzcw.js
@@ -0,0 +1,1 @@
+import{j as e,k as t,x as n}from"./vendor-D8Yhtuvn.js";import{t as r}from"./_plugin-vue_export-helper-S3RvzygF.js";var i={};function a(r,i){let a=e(`router-view`);return t(),n(a)}var o=r(i,[[`render`,a]]);export{o as default};
