add20459a2950b46cd943212d0dbf5c870014b2a
alex <766f6964+github@gmail.com>
2026-04-27 23:34:12 +0200

    added signup api route

--- vite-vue-ts-frontend/src/composables/api.ts
+++ vite-vue-ts-frontend/src/composables/api.ts
@@ -1,6 +1,23 @@
 import { axiosInstance } from "./axios";
 
 const api = {
+  auth: {
+    signIn: (email: string, password: string) => {
+      const params = {
+        email: email,
+        password: password,
+      };
+      return axiosInstance.post("/auth/signin", params);
+    },
+    signUp: (name: string, email: string, password: string) => {
+      const params = {
+        name: name,
+        email: email,
+        password: password,
+      };
+      return axiosInstance.post("/auth/signup", params);
+    },
+  },
   user: {
     search: () => axiosInstance.get("/users"),
   },
