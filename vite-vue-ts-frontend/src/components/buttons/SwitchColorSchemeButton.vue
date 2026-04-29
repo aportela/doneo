79d0a49c3f8e4d5fe3945def819255ed5c516454
alex <766f6964+github@gmail.com>
2026-04-29 22:18:25 +0200

    added new switch color scheme (light/dark) button

---
+++ vite-vue-ts-frontend/src/components/buttons/SwitchColorSchemeButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { NButton } from 'naive-ui';
+    import { IconMoon, IconSun } from '@tabler/icons-vue';
+    import { useColorSchemeStore } from '../../stores/colorScheme';
+
+    const colorSchemeStore = useColorSchemeStore();
+</script>
+
+<template>
+    <n-button quaternary>
+        <IconMoon v-if="colorSchemeStore.light" @click.prevent="colorSchemeStore.toggle" @mousedown.prevent />
+        <IconSun v-else @click="colorSchemeStore.toggle" @mousedown.prevent />
+    </n-button>
+</template>
+
+<style lang="css" scoped></style>
