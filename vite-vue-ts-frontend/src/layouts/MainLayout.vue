e479f59ab5baef5e649c0b045e28fcdef5fe7c5a
alex <766f6964+github@gmail.com>
2026-04-29 23:24:01 +0200

    old header removed

--- vite-vue-ts-frontend/src/layouts/MainLayout.vue
+++ vite-vue-ts-frontend/src/layouts/MainLayout.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
     import { default as TopHeader } from './TopHeader.vue';
     import { default as TopMenu } from './TopMenu.vue';
-    import Header from '../components/Header.vue';
 </script>
 
 <template>
     <TopHeader />
     <TopMenu />
-    <Header />
     <div class="page-wrapper">
         <div class="page-body">
             <div class="container-xl">
