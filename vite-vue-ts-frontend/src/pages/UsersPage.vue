4530c12d3851ae9e19bd5a7d25c0c17564451940
alex <766f6964+github@gmail.com>
2026-04-30 02:18:45 +0200

    use new naive ui data tables

--- vite-vue-ts-frontend/src/pages/UsersPage.vue
+++ vite-vue-ts-frontend/src/pages/UsersPage.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
-    import { onMounted, shallowRef } from 'vue';
+    import { h, onMounted, shallowRef } from 'vue';
     import { api } from '../composables/api';
-    import { IconEdit, IconTrash } from '@tabler/icons-vue';
+    import { NDataTable, NAvatar } from 'naive-ui';
+    import type { DataTableColumns } from 'naive-ui'
 
     interface UserInterface {
         id: string;
@@ -33,144 +34,60 @@
         }
     }
 
+    const columns: DataTableColumns<UserInterface> = [
+        {
+            title: 'Avatar',
+            key: 'avatar',
+            render(row) { return h(NAvatar, { src: "https://i.pravatar.cc/32?u=" + row.id, },) },
+        },
+        {
+            title: 'Name',
+            key: 'name'
+        },
+        {
+            title: 'Email',
+            key: 'email'
+        },
+        {
+            title: 'Admin',
+            key: 'isAdministrator',
+        },
+        {
+            title: 'Created At',
+            key: 'createdAt',
+            render(row) {
+                return new Date(row.createdAt).toLocaleString()
+            }
+        },
+        {
+            title: 'Last Updated',
+            key: 'lastUpdateAt',
+            render(row) {
+                if (row.lastUpdateAt) {
+                    return new Date(row.lastUpdateAt).toLocaleString()
+                } else {
+                    return null;
+                }
+            }
+        }
+    ];
+
     const users = shallowRef<User[]>([]);
 
     onMounted(() => {
         api.user.search().then((successResponse: any) => {
-            users.value = successResponse.data;
+            users.value = successResponse.data.users;
         }).catch((errorResponse: any) => {
             console.log(errorResponse);
         });
     });
 
+    const pagination = false as const
+
 </script>
 
 <template>
-    <div class="row row-deck row-cards">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Users</h3>
-                </div>
-                <div class="card-body border-bottom py-3">
-                    <div class="d-flex">
-                        <div class="text-secondary">
-                            Show
-                            <div class="mx-2 d-inline-block">
-                                <input type="text" class="form-control form-control-sm" value="8" size="3"
-                                    aria-label="Invoices count">
-                            </div>
-                            entries
-                        </div>
-                        <div class="ms-auto text-secondary">
-                            Search:
-                            <div class="ms-2 d-inline-block">
-                                <input type="text" class="form-control form-control-sm" aria-label="Search invoice">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="table-responsive">
-                    <table class="table table-selectable card-table table-vcenter text-nowrap datatable">
-                        <thead>
-                            <tr>
-                                <th class="w-1">
-                                    Type
-                                    <!-- Download SVG icon from http://tabler.io/icons/icon/chevron-up -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="icon icon-sm icon-thick icon-2">
-                                        <path d="M6 15l6 -6l6 6"></path>
-                                    </svg>
-                                </th>
-                                <th class="text-center">Avatar</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Created at</th>
-                                <th>Last update</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="user in users" v-bind:key="user.id">
-                                <td>
-                                    {{ user.isAdministrator ? 'Administrator' : 'User'
-                                    }}
-                                </td>
-                                <td class="text-center">
-                                    <span class="avatar" :style="'background-image: url(' + user.avatar + ')'"></span>
-                                </td>
-                                <td>{{ user.name }}</td>
-                                <td><a :href="'mailto:' + user.email">{{ user.email }}</a></td>
-                                <td><span class="text-secondary">{{ new Date(user.createdAt).toDateString() }}</span>
-                                </td>
-                                <td><span class="text-secondary" v-if="user.lastUpdateAt">{{ new
-                                    Date(user.lastUpdateAt).toDateString() }}</span></td>
-                                <td>
-                                    <div class="btn-actions">
-                                        <a href="#" class="btn btn-action btn-icon" aria-label="Button">
-                                            <IconEdit /> Edit
-                                        </a>
-                                        <a href="#" class="btn btn-action btn-icon ms-2" aria-label="Button">
-                                            <IconTrash /> Remove
-                                        </a>
-                                    </div>
-                                </td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-                </div>
-                <div class="card-footer">
-                    <div class="row g-2 justify-content-center justify-content-sm-between">
-                        <div class="col-auto d-flex align-items-center">
-                            <p class="m-0 text-secondary">Showing <strong>1 to 8</strong> of <strong>16 entries</strong>
-                            </p>
-                        </div>
-                        <div class="col-auto">
-                            <ul class="pagination m-0 ms-auto">
-                                <li class="page-item disabled">
-                                    <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-                                        <!-- Download SVG icon from http://tabler.io/icons/icon/chevron-left -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                                            <path d="M15 6l-6 6l6 6"></path>
-                                        </svg>
-                                    </a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">1</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">2</a>
-                                </li>
-                                <li class="page-item active">
-                                    <a class="page-link" href="#">3</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">4</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">5</a>
-                                </li>
-                                <li class="page-item">
-                                    <a class="page-link" href="#">
-                                        <!-- Download SVG icon from http://tabler.io/icons/icon/chevron-right -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                            stroke-linecap="round" stroke-linejoin="round" class="icon icon-1">
-                                            <path d="M9 6l6 6l-6 6"></path>
-                                        </svg>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <n-data-table :columns="columns" :data="users" :pagination="pagination" :bordered="false" />
 </template>
 
 <style lang="css"></style>
