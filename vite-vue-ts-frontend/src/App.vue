eec5caf354f3c578dbdd528996c29d418acd2640
alex <766f6964+github@gmail.com>
2026-05-01 17:22:57 +0200

    allow change color scheme

--- vite-vue-ts-frontend/src/App.vue
+++ vite-vue-ts-frontend/src/App.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
   import { NConfigProvider } from 'naive-ui';
+  import { darkTheme } from 'naive-ui'
+
+  import { useColorSchemeStore } from './stores/colorScheme';
+
+  const colorSchemeStore = useColorSchemeStore();
 
 </script>
 
 <template>
-  <n-config-provider>
+  <n-config-provider :theme="colorSchemeStore.dark ? darkTheme : null">
     <router-view />
   </n-config-provider>
 </template>
