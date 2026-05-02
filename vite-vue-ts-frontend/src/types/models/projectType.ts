39f4fa65278a093e33b9445aeb64a4800831c3e2
alex <766f6964+github@gmail.com>
2026-05-02 16:11:05 +0200

    moved project type interface/class to external file

---
+++ vite-vue-ts-frontend/src/types/models/projectType.ts
@@ -0,0 +1,21 @@
+interface ProjectTypeInterface {
+  id: string;
+  name: string;
+  index: number;
+  hexColor: string;
+}
+
+class ProjectTypeClass implements ProjectTypeInterface {
+  id: string;
+  name: string;
+  index: number;
+  hexColor: string;
+  constructor(item: ProjectTypeInterface) {
+    this.id = item.id;
+    this.name = item.name;
+    this.index = item.index;
+    this.hexColor = item.hexColor;
+  }
+}
+
+export { type ProjectTypeInterface, type ProjectTypeClass };
