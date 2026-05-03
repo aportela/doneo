d04d3b0c7f10c43a990d55c67e81308d136c211d
alex <766f6964+github@gmail.com>
2026-05-03 15:11:03 +0200

    new notifications button & composable

---
+++ vite-vue-ts-frontend/src/components/buttons/SwitchNotificationsButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+    import { useI18n } from "vue-i18n";
+    import { NButton, NTooltip } from 'naive-ui';
+    import { IconBell, IconBellOff } from '@tabler/icons-vue';
+    import { useUserSettingsStore } from '../../stores/userSettings';
+
+    const { t } = useI18n();
+
+    interface SwitchNotificationsButtonProps {
+        size?: number,
+    };
+
+    withDefaults(defineProps<SwitchNotificationsButtonProps>(), {
+        size: 20
+    });
+
+    const userSettingsStore = useUserSettingsStore();
+</script>
+
+<template>
+    <n-tooltip trigger="hover">
+        <template #trigger>
+            <n-button quaternary hint>
+                <IconBellOff :size="size" v-if="userSettingsStore.disableNotifications"
+                    @click.prevent="userSettingsStore.toggleNotifications" @mousedown.prevent />
+                <IconBell :size="size" v-else @click="userSettingsStore.toggleNotifications" @mousedown.prevent />
+            </n-button>
+        </template>
+        {{
+            t(userSettingsStore.disableNotifications ?
+                "Enable notifications" :
+                "Disable notifications")
+        }}
+    </n-tooltip>
+</template>
+
+<style lang="css" scoped></style>
--- vite-vue-ts-frontend/src/composables/notification.ts
+++ vite-vue-ts-frontend/src/composables/notification.ts
@@ -1,15 +1,20 @@
 import { type NotificationType, useNotification } from "naive-ui";
 
+import { useUserSettingsStore } from "../stores/userSettings";
+
 export const useNotify = () => {
+  const userSettingsStore = useUserSettingsStore();
   const notification = useNotification();
 
   const notify = (type: NotificationType, content: string, meta: string) => {
-    notification[type]({
-      content,
-      meta,
-      duration: 2500,
-      keepAliveOnHover: true,
-    });
+    if (userSettingsStore.hasNotificationsEnabled) {
+      notification[type]({
+        content,
+        meta,
+        duration: 2500,
+        keepAliveOnHover: true,
+      });
+    }
   };
 
   return { notify };
