44a85edb6cea984ef43d38869cdaa9346947f303
alex <766f6964+github@gmail.com>
2026-05-05 21:24:45 +0200

    added sidebar brand

--- vite-vue-ts-frontend/src/layouts/SidebarMenu.vue
+++ vite-vue-ts-frontend/src/layouts/SidebarMenu.vue
@@ -25,22 +25,6 @@
 
     const menuOptions: MenuOption[] = [
         {
-            label: () => h(
-                "div",
-                {
-                    style: {
-                        fontWeight: "bold",
-                        cursor: "default",
-                        opacity: 1,
-                    }
-                },
-                "Doneo"
-            ),
-            key: "brand",
-            icon: renderIcon(IconDatabaseStar)(commonIconSize),
-            disabled: true,
-        },
-        {
             key: 'divider-1',
             type: 'divider',
             props: {
@@ -242,8 +226,27 @@
 </script>
 
 <template>
+    <div class="brand-container">
+        <IconDatabaseStar :size="32" class="brand-icon" />
+        <span class="brand-name">Doneo</span>
+    </div>
     <n-menu :collapsed-width="64" :collapsed-icon-size="commonIconSize" :options="menuOptions"
         :value="route.name as string" accordion />
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    .brand-container {
+        padding-left: 32px;
+        display: flex;
+        align-items: center;
+    }
+
+    .brand-name {
+        font-weight: bold;
+        font-size: 30px;
+    }
+
+    .brand-icon {
+        margin-right: 8px;
+    }
+</style>
