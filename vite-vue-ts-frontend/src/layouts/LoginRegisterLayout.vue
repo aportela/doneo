5eb4b3608325c845d44a80622c8f025808e84c8e
alex <766f6964+github@gmail.com>
2026-05-10 01:53:58 +0200

    added refresh token refresh & redirect on sucess

--- vite-vue-ts-frontend/src/layouts/LoginRegisterLayout.vue
+++ vite-vue-ts-frontend/src/layouts/LoginRegisterLayout.vue
@@ -1,6 +1,32 @@
 <script setup lang="ts">
+    import { ref, onMounted } from "vue";
+    import { useRoute, useRouter } from 'vue-router';
+    import { useSessionStore } from "../stores/session";
+    import { TokenManager } from '../api/services/tokenManager';
+
+    const router = useRouter();
+    const route = useRoute();
+    const sessionStore = useSessionStore();
+
+    const tokenRefreshing = ref<boolean>(false);
+    onMounted(async () => {
+        if (!sessionStore.hasAccessToken) {
+            tokenRefreshing.value = true;
+            try {
+                const success = await TokenManager.refreshAccessToken(sessionStore);
+                if (success) {
+                    const redirectPath = (route.query.redirect as string) || '/home';
+                    await router.push(redirectPath);
+                    tokenRefreshing.value = false;
+                }
+            } catch (error: unknown) {
+                console.error("An unhandled exception occurred during access token refresh", error);
+                tokenRefreshing.value = false;
+            }
+        }
+    });
 </script>
 
 <template>
-    <router-view />
+    <router-view v-if="!tokenRefreshing" />
 </template>
