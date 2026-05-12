7bc7f49d416911199f3b166241a57b5457c8a5e6
alex <766f6964+github@gmail.com>
2026-05-12 22:20:40 +0200

    added simple email validator without dependencies

--- vite-vue-ts-frontend/src/shared/composables/form-validators.ts
+++ vite-vue-ts-frontend/src/shared/composables/form-validators.ts
@@ -47,3 +47,8 @@ export const validEmail: Validator = (value: string) => {
   }
   return true;
 };
+
+export const isValidEmail = (email: string) => {
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return emailRegex.test(email);
+};
