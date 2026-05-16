5c169ea630af1b3cccdb1df42793adf820a9d6eb
alex <766f6964+github@gmail.com>
2026-05-16 10:07:56 +0200

    added new column props

--- vite-vue-ts-frontend/src/shared/types/table-header-column.ts
+++ vite-vue-ts-frontend/src/shared/types/table-header-column.ts
@@ -1,4 +1,8 @@
+type TableHeaderColumnAlign = "left" | "right" | "center";
+
 export interface TableHeaderColumn {
   label: string;
   field: string;
+  sortable: boolean;
+  align?: TableHeaderColumnAlign;
 }
