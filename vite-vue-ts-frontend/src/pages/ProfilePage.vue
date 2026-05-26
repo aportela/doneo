804e6350aceb1ff0666c3ba553858304a1cb49af
alex <766f6964+github@gmail.com>
2026-05-26 15:30:38 +0200

    avatar url replaced

--- vite-vue-ts-frontend/src/pages/ProfilePage.vue
+++ vite-vue-ts-frontend/src/pages/ProfilePage.vue
@@ -17,8 +17,7 @@
                 <h1>My account</h1>
                 <h2>Profile details</h2>
                 <n-flex style="align-items:center;">
-
-                    <n-avatar :size="128" src="https://i.pravatar.cc/128?u=019dfcaa-aa06-755b-aca0-bb05e4ea44a7" />
+                    <n-avatar :size="128" src="/api/avatars/128/user/019dfcaa-aa06-755b-aca0-bb05e4ea44a7" />
                     <n-button>
                         <template #icon>
                             <n-icon>
