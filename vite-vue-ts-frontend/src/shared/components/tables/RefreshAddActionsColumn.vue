6c3efe03820a177e977f67776012d64916142422
alex <766f6964+github@gmail.com>
2026-06-03 21:34:38 +0200

    added props for button size & defaults

--- vite-vue-ts-frontend/src/shared/components/tables/RefreshAddActionsColumn.vue
+++ vite-vue-ts-frontend/src/shared/components/tables/RefreshAddActionsColumn.vue
@@ -2,22 +2,23 @@
 
     import { useI18n } from "vue-i18n";
 
-    import { NButtonGroup, NButton, NIcon } from 'naive-ui';
+    import { NButtonGroup, NButton, NIcon, type ButtonSize } from 'naive-ui';
     import { IconRefresh, IconPlus, IconSettings } from '@tabler/icons-vue';
 
     interface RefreshAddActionsColumnProps {
         disabled?: boolean;
+        buttonSize?: ButtonSize
         iconSize?: number;
         hideRefresh?: boolean;
         hideAdd?: boolean;
         hideSettings?: boolean;
-
     }
 
     const emit = defineEmits(['refresh', 'add', 'settings'])
 
     const props = withDefaults(defineProps<RefreshAddActionsColumnProps>(), {
         disabled: false,
+        size: "small",
         iconSize: 22,
         hideRefresh: false,
         hideAdd: false,
@@ -40,26 +41,36 @@
 </script>
 
 <template>
-    <n-button-group size="small" style="width: 100%">
-        <n-button @click="onRefresh" v-if="!props.hideRefresh" style="flex: 1;">
+    <n-button-group :size="props.buttonSize" class="doneo-table-actions-button-group">
+        <n-button @click="onRefresh" :disabled="props.disabled" v-if="!props.hideRefresh"
+            class="doneo-table-actions-button">
             <template #icon>
-                <n-icon :size="22" :component="IconRefresh" />
+                <n-icon :size="props.iconSize" :component="IconRefresh" />
             </template>
             {{ t("shared.buttons.Refresh.label") }}
         </n-button>
-        <n-button @click="onAdd" v-if="!props.hideAdd" style="flex: 1;">
+        <n-button @click="onAdd" :disabled="props.disabled" v-if="!props.hideAdd" class="doneo-table-actions-button">
             <template #icon>
-                <n-icon :size="22" :component="IconPlus" />
+                <n-icon :size="props.iconSize" :component="IconPlus" />
             </template>
             {{ t("shared.buttons.Add.label") }}
         </n-button>
-        <n-button @click="onSettings" v-if="!props.hideSettings" style="flex: 1;">
+        <n-button @click="onSettings" :disabled="props.disabled" v-if="!props.hideSettings"
+            class="doneo-table-actions-button">
             <template #icon>
-                <n-icon :size="22" :component="IconSettings" />
+                <n-icon :size="props.iconSize" :component="IconSettings" />
             </template>
             {{ t("shared.buttons.Settings.label") }}
         </n-button>
     </n-button-group>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+    .doneo-table-actions-button-group {
+        width: 100%;
+    }
+
+    .doneo-table-actions-button {
+        flex: 1;
+    }
+</style>
