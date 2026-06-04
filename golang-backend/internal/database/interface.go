26287a9410be159330f2cacbfb9d65670ff7ee36
alex <766f6964+github@gmail.com>
2026-06-04 23:54:43 +0200

    new database interface for using transactions

--- golang-backend/internal/database/interface.go
+++ golang-backend/internal/database/interface.go
@@ -16,3 +16,15 @@ type Database interface {
 	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
 	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
 }
+
+type DB interface {
+	Open(cfg config.DatabaseConfiguration) error
+	Close() error
+	Begin(ctx context.Context) (*sql.Tx, error)
+}
+
+type DBTX interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
+}
