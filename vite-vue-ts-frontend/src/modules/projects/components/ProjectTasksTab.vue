a855514cb11c603348212d333b6d2cfde39c0a66
alex <766f6964+github@gmail.com>
2026-06-05 23:43:46 +0200

    added columns definition

--- vite-vue-ts-frontend/src/modules/projects/components/ProjectTasksTab.vue
+++ vite-vue-ts-frontend/src/modules/projects/components/ProjectTasksTab.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-    import type { CSSProperties } from "vue";
-    import { useI18n } from "vue-i18n";
+    import { computed, type CSSProperties } from "vue";
+    //import { useI18n } from "vue-i18n";
 
     import { NCard } from "naive-ui";
-
 
     import { UserBase } from "../../users/models/user.ts";
 
     import AvatarUserName from "../../../shared/components/AvatarUserName.vue";
     import ManageTable from '../../../shared/components/tables/ManageTable.vue';
     import ManageTableActionButtons from '../../../shared/components/tables/ManageTableActionButtons.vue';
+    import type { TableHeaderColumn } from "../../../shared/types/table-header-column.ts";
 
     interface ProjectTasksProps {
         style?: string | CSSProperties;
@@ -18,27 +18,64 @@
 
     const props = defineProps<ProjectTasksProps>();
 
-    const { t } = useI18n();
+    //const { t } = useI18n();
 
     const user = new UserBase({ id: '019e5b68-f701-7af7-a694-c8146e3366a1', name: 'John Doe' });
 
     const itemCount = defineModel<number>("itemCount", { default: 0 });
     itemCount.value = 0;
+
+    const columns = computed<TableHeaderColumn[]>(() => [
+        {
+            label: "Identifier",
+            field: "id",
+            visible: true,
+            sortable: false,
+            isFiltered: () => false,
+        },
+        {
+            label: "Summary",
+            field: "summary",
+            visible: true,
+            sortable: false,
+            isFiltered: () => false,
+        },
+        {
+            label: "Status",
+            field: "status",
+            visible: true,
+            sortable: false,
+            isFiltered: () => false,
+        },
+        {
+            label: "Created at",
+            field: "createdAt",
+            visible: true,
+            sortable: false,
+            isFiltered: () => false,
+        },
+        {
+            label: "Created by",
+            field: "createdBy",
+            visible: true,
+            sortable: false,
+            isFiltered: () => false,
+        },
+    ]);
+
+    const onRefresh = () => {
+
+    };
+
+    const onAdd = () => {
+
+    };
 </script>
 
 <template>
     <n-card bordered :style="props.style">
-        <ManageTable size="small" :columns="[]">
+        <ManageTable size="small" :columns="columns" @refresh="onRefresh" @add="onAdd">
             <template #thead>
-                <tr>
-                    <th>Identifier</th>
-                    <th>Summary</th>
-                    <th>Status</th>
-                    <th>Created at</th>
-                    <th>Created by</th>
-                    <th class="doneo-table-actions-column">{{
-                        t("shared.components.table.header.columns.actions") }}</th>
-                </tr>
             </template>
             <template #tbody>
                 <tr v-for="index in [1, 2, 3, 4, 5, 6, 7, 8, 9]" :key="index">
