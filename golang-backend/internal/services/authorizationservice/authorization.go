cfa454b2c25aefcbd8743e8df1747a4fd3b30709
alex <766f6964+github@gmail.com>
2026-06-14 15:55:42 +0200

    check admin user permissions before project user permissions

--- golang-backend/internal/services/authorizationservice/authorization.go
+++ golang-backend/internal/services/authorizationservice/authorization.go
@@ -7,6 +7,7 @@ import (
 	"github.com/aportela/doneo/internal/database"
 	"github.com/aportela/doneo/internal/domain"
 	"github.com/aportela/doneo/internal/repositories/projectpermissionrepository"
+	"github.com/aportela/doneo/internal/repositories/userrepository"
 	"github.com/aportela/doneo/internal/services/cacheservice"
 )
 
@@ -22,27 +23,47 @@ type AuthorizationService interface {
 }
 
 type authorizationService struct {
-	database   database.Database // TODO: delete ||create project permission repository with this on NewService
-	cache      cacheservice.ProjectPermissionCache
-	repository projectpermissionrepository.ProjectPermissionRepository
+	database                    database.Database
+	projectPermissionCache      cacheservice.ProjectPermissionCache
+	userPermissionCache         cacheservice.UserPermissionCache
+	userRepository              userrepository.UserRepository
+	projectPermissionRepository projectpermissionrepository.ProjectPermissionRepository
 }
 
-func NewService(database database.Database, repository projectpermissionrepository.ProjectPermissionRepository) AuthorizationService {
-	return &authorizationService{database: database, cache: cacheservice.NewProjectPermissionCache(), repository: repository}
+func NewService(database database.Database) AuthorizationService {
+	return &authorizationService{
+		database:                    database,
+		projectPermissionCache:      cacheservice.NewProjectPermissionCache(),
+		userPermissionCache:         cacheservice.NewUserPermissionCache(),
+		userRepository:              userrepository.NewRepository(database),
+		projectPermissionRepository: projectpermissionrepository.NewRepository(database),
+	}
 }
 
 func (service *authorizationService) RequireProjectPermission(ctx context.Context, userID string, projectID string, permission domain.Bitmask) error {
-	permissionsBitmask, ok := service.cache.Get(userID, projectID)
+	userPermissionsBitmask, ok := service.userPermissionCache.Get(userID)
+	if !ok {
+		user, err := service.userRepository.Get(ctx, userID)
+		if err != nil {
+			return fmt.Errorf("[AuthorizationService] failed to get user permissions: %w", err)
+		}
+		userPermissionsBitmask = user.PermissionsBitmask
+		service.userPermissionCache.Set(userID, userPermissionsBitmask)
+	}
+	if userPermissionsBitmask.HasFlag(domain.UserPermissionAdmin) {
+		return nil
+	}
+	permissionsBitmask, ok := service.projectPermissionCache.Get(userID, projectID)
 	if !ok {
 		found := false
-		projectPermissions, err := service.repository.Search(ctx, projectID)
+		projectPermissions, err := service.projectPermissionRepository.Search(ctx, projectID)
 		if err != nil {
 			return fmt.Errorf("[AuthorizationService] failed to get project permissions: %w", err)
 		}
 		for _, projectPermission := range projectPermissions {
 			if projectPermission.User.ID == userID {
 				permissionsBitmask = projectPermission.Role.PermissionsBitmask
-				service.cache.Set(userID, projectID, permissionsBitmask)
+				service.projectPermissionCache.Set(userID, projectID, permissionsBitmask)
 				found = true
 			}
 		}
@@ -52,9 +73,8 @@ func (service *authorizationService) RequireProjectPermission(ctx context.Contex
 	}
 	if permissionsBitmask.HasFlag(permission) {
 		return nil
-	} else {
-		return domain.AuthorizationError
 	}
+	return domain.AuthorizationError
 }
 
 func (service *authorizationService) RequireProjectUpdatePermission(ctx context.Context, userID string, projectID string) error {
