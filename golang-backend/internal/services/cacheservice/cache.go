d0cf29b8a8c993d15024798d492a1c90e4d05f2b
alex <766f6964+github@gmail.com>
2026-06-14 15:37:13 +0200

    added user permission cache

--- golang-backend/internal/services/cacheservice/cache.go
+++ golang-backend/internal/services/cacheservice/cache.go
@@ -6,6 +6,64 @@ import (
 	"github.com/aportela/doneo/internal/domain"
 )
 
+type UserPermissionCacheKey struct {
+	UserID string
+}
+
+type UserPermissionCache interface {
+	Get(userID string) (domain.Bitmask, bool)
+	Set(userID string, permissionBitmask domain.Bitmask)
+	Delete(userID string)
+	Clear()
+}
+
+type userPermissionCache struct {
+	mu          sync.RWMutex
+	permissions map[UserPermissionCacheKey]domain.Bitmask
+}
+
+func NewUserPermissionCache() UserPermissionCache {
+	return &userPermissionCache{
+		permissions: make(map[UserPermissionCacheKey]domain.Bitmask),
+	}
+}
+
+func (service *userPermissionCache) Get(userID string) (domain.Bitmask, bool) {
+	service.mu.RLock()
+	defer service.mu.RUnlock()
+
+	permissionBitmask, ok := service.permissions[UserPermissionCacheKey{
+		UserID: userID,
+	}]
+
+	return permissionBitmask, ok
+}
+
+func (service *userPermissionCache) Set(userID string, permissionBitmask domain.Bitmask) {
+	service.mu.Lock()
+	defer service.mu.Unlock()
+
+	service.permissions[UserPermissionCacheKey{
+		UserID: userID,
+	}] = permissionBitmask
+}
+
+func (service *userPermissionCache) Delete(userID string) {
+	service.mu.Lock()
+	defer service.mu.Unlock()
+
+	delete(service.permissions, UserPermissionCacheKey{
+		UserID: userID,
+	})
+}
+
+func (service *userPermissionCache) Clear() {
+	service.mu.Lock()
+	defer service.mu.Unlock()
+
+	service.permissions = make(map[UserPermissionCacheKey]domain.Bitmask)
+}
+
 type ProjectPermissionCacheKey struct {
 	UserID    string
 	ProjectID string
