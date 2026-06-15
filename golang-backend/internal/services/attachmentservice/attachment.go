65890cff0cf4571e46863c1e260001c17b36f58e
alex <766f6964+github@gmail.com>
2026-06-16 00:58:01 +0200

    attachment service updated for new repository changes

--- golang-backend/internal/services/attachmentservice/attachment.go
+++ golang-backend/internal/services/attachmentservice/attachment.go
@@ -2,6 +2,7 @@ package attachmentservice
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
@@ -9,182 +10,192 @@ import (
 	"github.com/aportela/doneo/internal/domain"
 	"github.com/aportela/doneo/internal/middlewares"
 	"github.com/aportela/doneo/internal/repositories/attachmentrepository"
+	"github.com/aportela/doneo/internal/services/authorizationservice"
 	"github.com/aportela/doneo/internal/services/historyoperationservice"
 	"github.com/aportela/doneo/internal/utils"
 )
 
 type AttachmentService interface {
-	GetAttachment(ctx context.Context, id string) (domain.Attachment, error)
-	AddProjectAttachment(ctx context.Context, projectId string, attachment domain.Attachment) (domain.Attachment, error)
-	DeleteProjectAttachment(ctx context.Context, projectId string, attachmentId string) error
-	GetProjectAttachments(ctx context.Context, projectId string) ([]domain.Attachment, error)
-	AddTaskAttachment(ctx context.Context, projectId string, taskId string, attachment domain.Attachment) (domain.Attachment, error)
-	DeleteTaskAttachment(ctx context.Context, projectId string, taskId string, attachmentId string) error
-	GetTaskAttachments(ctx context.Context, taskId string) ([]domain.Attachment, error)
+	GetAttachment(ctx context.Context, attachmentID string) (domain.Attachment, error)
+
+	AddProjectAttachment(ctx context.Context, projectID string, attachment domain.Attachment) (domain.Attachment, error)
+	DeleteProjectAttachment(ctx context.Context, projectID string, attachmentID string) error
+	GetProjectAttachments(ctx context.Context, projectID string) ([]domain.Attachment, error)
+
+	AddTaskAttachment(ctx context.Context, projectID string, taskID string, attachment domain.Attachment) (domain.Attachment, error)
+	DeleteTaskAttachment(ctx context.Context, projectID string, taskID string, attachmentID string) error
+	GetTaskAttachments(ctx context.Context, taskID string) ([]domain.Attachment, error)
 }
 
 type attachmentService struct {
-	database                database.Database
+	db                      database.Database
+	authorizationService    authorizationservice.AuthorizationService
 	historyOperationService historyoperationservice.HistoryOperationService
-	repository              attachmentrepository.AttachmentRepository
+	attachmentRepository    attachmentrepository.AttachmentRepository
 }
 
-func NewService(db database.Database, historyOperationService historyoperationservice.HistoryOperationService, repository attachmentrepository.AttachmentRepository) AttachmentService {
-	return &attachmentService{database: db, historyOperationService: historyOperationService, repository: repository}
+func NewService(db database.Database, authorizationService authorizationservice.AuthorizationService, historyOperationService historyoperationservice.HistoryOperationService, attachmentRepository attachmentrepository.AttachmentRepository) AttachmentService {
+	return &attachmentService{db: db, authorizationService: authorizationService, historyOperationService: historyOperationService, attachmentRepository: attachmentRepository}
 }
 
-func (service *attachmentService) GetAttachment(ctx context.Context, id string) (domain.Attachment, error) {
-	attachment, err := service.repository.GetAttachment(ctx, id)
-	if err != nil {
-		return domain.Attachment{}, fmt.Errorf("[AttachmentService] failed to get attachment with ID %s: %w", id, err)
-	}
-	return attachment, nil
-}
-
-func (service *attachmentService) AddProjectAttachment(ctx context.Context, projectId string, attachment domain.Attachment) (domain.Attachment, error) {
-	tx, err := service.database.Begin()
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
-		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
+func (service *attachmentService) withProjectUpdatePermission(ctx context.Context, projectID string, action func(userID string) error) error {
+	userID, ok := middlewares.GetUserIDFromContext(ctx)
 	if !ok {
-		return domain.Attachment{}, fmt.Errorf("[AttachmentService] user ID not found in context")
+		return fmt.Errorf("user not found in context")
 	}
-	attachment.CreatedBy.ID = currentUserId
-	attachment.CreatedAt = time.Now()
-	err = attachmentrepository.NewRepository(service.database).AddAttachment(ctx, attachment)
+
+	if err := service.authorizationService.RequireProjectUpdatePermission(ctx, userID, projectID); err != nil {
+		return err
+	}
+
+	return action(userID)
+}
+
+func (service *attachmentService) GetAttachment(ctx context.Context, attachmentID string) (domain.Attachment, error) {
+	// TODO: check permissions
+	attachment, err := service.attachmentRepository.GetAttachment(ctx, service.db, attachmentID)
 	if err != nil {
-		return domain.Attachment{}, err
+		return domain.Attachment{}, fmt.Errorf("[AttachmentService] failed to get attachment with ID %s: %w", attachmentID, err)
 	}
-	err = service.repository.AddProjectAttachment(ctx, projectId, attachment.ID)
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	_, err = service.historyOperationService.AddProjectHistoryOperation(ctx, tx, projectId, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: attachment.CreatedAt, OperationType: domain.EventProjectAttachmentAdded})
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	err = tx.Commit()
+	return attachment, nil
+}
+
+func (service *attachmentService) AddProjectAttachment(ctx context.Context, projectID string, attachment domain.Attachment) (domain.Attachment, error) {
+	err := service.withProjectUpdatePermission(ctx, projectID, func(currentUserID string) error {
+		attachment.CreatedBy.ID = currentUserID
+		attachment.CreatedAt = time.Now()
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.attachmentRepository.AddAttachment(ctx, tx, attachment); err != nil {
+				return err
+			}
+			if err := service.attachmentRepository.AddProjectAttachment(ctx, tx, projectID, attachment.ID); err != nil {
+				return err
+			}
+			if _, err := service.historyOperationService.AddProjectHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     attachment.CreatedAt,
+					OperationType: domain.EventProjectAttachmentAdded,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
 	if err != nil {
 		return domain.Attachment{}, err
 	}
 	return attachment, nil
 }
 
-func (service *attachmentService) DeleteProjectAttachment(ctx context.Context, projectId string, attachmentId string) error {
+func (service *attachmentService) DeleteProjectAttachment(ctx context.Context, projectID string, attachmentID string) error {
 	// TODO: remove data/attachments file from storage
-	tx, err := service.database.Begin()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
-		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
-	if !ok {
-		return fmt.Errorf("[AttachmentService] user ID not found in context")
-	}
-	err = service.repository.DeleteProjectAttachment(ctx, projectId, attachmentId)
-	if err != nil {
-		return err
-	}
-	_, err = service.historyOperationService.AddProjectHistoryOperation(ctx, tx, projectId, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: time.Now(), OperationType: domain.EventProjectAttachmentDeleted})
-	if err != nil {
-		return err
-	}
-	return tx.Commit()
+	err := service.withProjectUpdatePermission(ctx, projectID, func(currentUserID string) error {
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.attachmentRepository.DeleteProjectAttachment(ctx, tx, projectID, attachmentID); err != nil {
+				return err
+			}
+			if err := service.attachmentRepository.DeleteAttachment(ctx, tx, attachmentID); err != nil {
+				return err
+			}
+			if _, err := service.historyOperationService.AddProjectHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     time.Now(),
+					OperationType: domain.EventProjectAttachmentDeleted,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
+	return err
 }
 
-func (service *attachmentService) GetProjectAttachments(ctx context.Context, projectId string) ([]domain.Attachment, error) {
-	attachments, err := service.repository.GetProjectAttachments(ctx, projectId)
+func (service *attachmentService) GetProjectAttachments(ctx context.Context, projectID string) ([]domain.Attachment, error) {
+	attachments, err := service.attachmentRepository.GetProjectAttachments(ctx, service.db, projectID)
 	if err != nil {
 		return nil, fmt.Errorf("[AttachmentService] failed to get project attachments: %w", err)
 	}
 	return attachments, nil
 }
 
-func (service *attachmentService) AddTaskAttachment(ctx context.Context, projectId string, taskId string, attachment domain.Attachment) (domain.Attachment, error) {
-	tx, err := service.database.Begin()
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
-		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
-	if !ok {
-		return domain.Attachment{}, fmt.Errorf("[AttachmentService] user ID not found in context")
-	}
-	attachment.CreatedBy.ID = currentUserId
-	attachment.CreatedAt = time.Now()
-	err = attachmentrepository.NewRepository(service.database).AddAttachment(ctx, attachment)
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	err = service.repository.AddTaskAttachment(ctx, taskId, attachment.ID)
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	_, err = service.historyOperationService.AddTaskHistoryOperation(ctx, tx, projectId, taskId, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: attachment.CreatedAt, OperationType: domain.EventTaskAttachmentAdded})
-	if err != nil {
-		return domain.Attachment{}, err
-	}
-	err = tx.Commit()
+func (service *attachmentService) AddTaskAttachment(ctx context.Context, projectID string, taskID string, attachment domain.Attachment) (domain.Attachment, error) {
+	err := service.withProjectUpdatePermission(ctx, projectID, func(currentUserID string) error {
+		attachment.CreatedBy.ID = currentUserID
+		attachment.CreatedAt = time.Now()
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.attachmentRepository.AddAttachment(ctx, tx, attachment); err != nil {
+				return err
+			}
+			if err := service.attachmentRepository.AddTaskAttachment(ctx, tx, taskID, attachment.ID); err != nil {
+				return err
+			}
+			if _, err := service.historyOperationService.AddTaskHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				taskID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     attachment.CreatedAt,
+					OperationType: domain.EventTaskAttachmentAdded,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
 	if err != nil {
 		return domain.Attachment{}, err
 	}
 	return attachment, nil
 }
 
-func (service *attachmentService) DeleteTaskAttachment(ctx context.Context, projectId string, taskId string, attachmentId string) error {
+func (service *attachmentService) DeleteTaskAttachment(ctx context.Context, projectID string, taskID string, attachmentID string) error {
 	// TODO: remove data/attachments file from storage
-	tx, err := service.database.Begin()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
-		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
-	if !ok {
-		return fmt.Errorf("[AttachmentService] user ID not found in context")
-	}
-	err = service.repository.DeleteTaskAttachment(ctx, taskId, attachmentId)
-	if err != nil {
-		return err
-	}
-	_, err = service.historyOperationService.AddTaskHistoryOperation(ctx, tx, projectId, taskId, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: time.Now(), OperationType: domain.EventTaskAttachmentDeleted})
-	if err != nil {
-		return err
-	}
-	return tx.Commit()
+	err := service.withProjectUpdatePermission(ctx, projectID, func(currentUserID string) error {
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.attachmentRepository.DeleteTaskAttachment(ctx, tx, taskID, attachmentID); err != nil {
+				return err
+			}
+			if err := service.attachmentRepository.DeleteAttachment(ctx, tx, attachmentID); err != nil {
+				return err
+			}
+			if _, err := service.historyOperationService.AddTaskHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				taskID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     time.Now(),
+					OperationType: domain.EventTaskAttachmentDeleted,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
+	return err
 }
 
-func (service *attachmentService) GetTaskAttachments(ctx context.Context, taskId string) ([]domain.Attachment, error) {
-	attachments, err := service.repository.GetTaskAttachments(ctx, taskId)
+func (service *attachmentService) GetTaskAttachments(ctx context.Context, taskID string) ([]domain.Attachment, error) {
+	attachments, err := service.attachmentRepository.GetTaskAttachments(ctx, service.db, taskID)
 	if err != nil {
 		return nil, fmt.Errorf("[AttachmentService] failed to get task attachments: %w", err)
 	}
