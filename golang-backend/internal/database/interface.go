c67867d36ed90bc9cf75f5181c1be754d62cc508
alex <766f6964+github@gmail.com>
2026-06-15 18:07:10 +0200

    splitted database executor interface

--- golang-backend/internal/database/interface.go
+++ golang-backend/internal/database/interface.go
@@ -7,12 +7,16 @@ import (
 	"github.com/aportela/doneo/internal/config"
 )
 
+type DatabaseExecutor interface {
+	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
+	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
+	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
+}
+
 type Database interface {
 	Open(cfg config.DatabaseConfiguration) error
 	Begin() (*sql.Tx, error)
 	CheckSchema() error
 	Close() error
-	ExecContext(ctx context.Context, query string, args ...any) (sql.Result, error)
-	QueryContext(ctx context.Context, query string, args ...any) (*sql.Rows, error)
-	QueryRowContext(ctx context.Context, query string, args ...any) *sql.Row
+	DatabaseExecutor
 }
