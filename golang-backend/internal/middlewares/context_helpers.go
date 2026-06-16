caddee16668d808fcd4154f42a91e2fe76a38cfa
alex <766f6964+github@gmail.com>
2026-06-16 14:57:50 +0200

    added new context user struct

--- golang-backend/internal/middlewares/context_helpers.go
+++ golang-backend/internal/middlewares/context_helpers.go
@@ -1,8 +1,28 @@
 package middlewares
 
-import "context"
+import (
+	"context"
+
+	"github.com/aportela/doneo/internal/domain"
+)
 
 type contextKey string
+
+type ContextUser struct {
+	domain.UserBase
+	IsSystem bool
+}
+
+const contextUserKey contextKey = "user"
+
+func GetContextUser(ctx context.Context) (ContextUser, bool) {
+	user, ok := ctx.Value(contextUserKey).(ContextUser)
+	return user, ok
+}
+
+func SetContextUser(ctx context.Context, user ContextUser) context.Context {
+	return context.WithValue(ctx, contextUserKey, user)
+}
 
 const userIDKey contextKey = "userID"
 
