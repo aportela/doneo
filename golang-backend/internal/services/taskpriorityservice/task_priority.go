fdab5ffd3f22661babe223da8d5aecda86ca609d
alex <766f6964+github@gmail.com>
2026-06-16 02:12:05 +0200

    added permission checks

--- golang-backend/internal/services/taskpriorityservice/task_priority.go
+++ golang-backend/internal/services/taskpriorityservice/task_priority.go
@@ -7,30 +7,52 @@ import (
 	"github.com/aportela/doneo/internal/browser"
 	"github.com/aportela/doneo/internal/database"
 	"github.com/aportela/doneo/internal/domain"
+	"github.com/aportela/doneo/internal/middlewares"
 	"github.com/aportela/doneo/internal/repositories/taskpriorityrepository"
+	"github.com/aportela/doneo/internal/services/authorizationservice"
 	"github.com/aportela/doneo/internal/utils"
 )
 
 type TaskPriorityService interface {
 	Add(ctx context.Context, taskPriority domain.TaskPriority) (domain.TaskPriority, error)
 	Update(ctx context.Context, taskPriority domain.TaskPriority) (domain.TaskPriority, error)
-	Delete(ctx context.Context, id string) error
-	Get(ctx context.Context, id string) (domain.TaskPriority, error)
+	Delete(ctx context.Context, taskPriorityID string) error
+	Get(ctx context.Context, taskPriorityID string) (domain.TaskPriority, error)
 	Search(ctx context.Context, pager browser.Params, order browser.Order, filter domain.SearchTaskPrioritiesFilter) ([]domain.TaskPriority, browser.Result, error)
 }
 
 type taskPriorityService struct {
-	database   database.Database
-	repository taskpriorityrepository.TaskPriorityRepository
+	db                     database.Database
+	authorizationService   authorizationservice.AuthorizationService
+	taskPriorityRepository taskpriorityrepository.TaskPriorityRepository
 }
 
-func NewService(db database.Database, repository taskpriorityrepository.TaskPriorityRepository) TaskPriorityService {
-	return &taskPriorityService{database: db, repository: repository}
+func NewService(db database.Database, authorizationService authorizationservice.AuthorizationService, taskPriorityRepository taskpriorityrepository.TaskPriorityRepository) TaskPriorityService {
+	return &taskPriorityService{db: db, authorizationService: authorizationService, taskPriorityRepository: taskPriorityRepository}
+}
+
+func (service *taskPriorityService) withUserAdminPermission(ctx context.Context, action func(currentUserID string) error) error {
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return fmt.Errorf("user not found in context")
+	}
+
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		return err
+	}
+
+	return action(currentContextUserID)
 }
 
 func (service *taskPriorityService) Add(ctx context.Context, taskPriority domain.TaskPriority) (domain.TaskPriority, error) {
-	taskPriority.ID = utils.UUID()
-	err := service.repository.Add(ctx, taskPriority)
+	err := service.withUserAdminPermission(ctx, func(currentUserID string) error {
+		taskPriority.ID = utils.UUID()
+		err := service.taskPriorityRepository.Add(ctx, service.db, taskPriority)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		return domain.TaskPriority{}, err
 	}
@@ -38,27 +60,56 @@ func (service *taskPriorityService) Add(ctx context.Context, taskPriority domain
 }
 
 func (service *taskPriorityService) Update(ctx context.Context, taskPriority domain.TaskPriority) (domain.TaskPriority, error) {
-	err := service.repository.Update(ctx, taskPriority)
+	err := service.withUserAdminPermission(ctx, func(currentUserID string) error {
+		err := service.taskPriorityRepository.Update(ctx, service.db, taskPriority)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		return domain.TaskPriority{}, err
 	}
 	return taskPriority, nil
 }
 
-func (service *taskPriorityService) Delete(ctx context.Context, id string) error {
-	return service.repository.Delete(ctx, id)
+func (service *taskPriorityService) Delete(ctx context.Context, taskPriorityID string) error {
+	err := service.withUserAdminPermission(ctx, func(currentUserID string) error {
+		err := service.taskPriorityRepository.Delete(ctx, service.db, taskPriorityID)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
 }
 
-func (service *taskPriorityService) Get(ctx context.Context, id string) (domain.TaskPriority, error) {
-	taskPriority, err := service.repository.Get(ctx, id)
+func (service *taskPriorityService) Get(ctx context.Context, taskPriorityID string) (domain.TaskPriority, error) {
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return domain.TaskPriority{}, fmt.Errorf("user not found in context")
+	}
+
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		return domain.TaskPriority{}, err
+	}
+	taskPriority, err := service.taskPriorityRepository.Get(ctx, service.db, taskPriorityID)
 	if err != nil {
-		return domain.TaskPriority{}, fmt.Errorf("[TaskPriorityService] failed to get task priority with ID %s: %w", id, err)
+		return domain.TaskPriority{}, fmt.Errorf("[TaskPriorityService] failed to get task priority with ID %s: %w", taskPriorityID, err)
 	}
 	return taskPriority, nil
 }
 
 func (service *taskPriorityService) Search(ctx context.Context, pager browser.Params, order browser.Order, filter domain.SearchTaskPrioritiesFilter) ([]domain.TaskPriority, browser.Result, error) {
-	taskPriorities, pagerResult, err := service.repository.Search(ctx, pager, order, filter)
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return nil, browser.Result{}, fmt.Errorf("user not found in context")
+	}
+
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		return nil, browser.Result{}, err
+	}
+	taskPriorities, pagerResult, err := service.taskPriorityRepository.Search(ctx, service.db, pager, order, filter)
 	if err != nil {
 		return nil, browser.Result{}, fmt.Errorf("[TaskPriorityService] failed to search task priorities: %w", err)
 	}
--- golang-backend/internal/services/taskstatusservice/task_status.go
+++ golang-backend/internal/services/taskstatusservice/task_status.go
@@ -7,30 +7,52 @@ import (
 	"github.com/aportela/doneo/internal/browser"
 	"github.com/aportela/doneo/internal/database"
 	"github.com/aportela/doneo/internal/domain"
+	"github.com/aportela/doneo/internal/middlewares"
 	"github.com/aportela/doneo/internal/repositories/taskstatusrepository"
+	"github.com/aportela/doneo/internal/services/authorizationservice"
 	"github.com/aportela/doneo/internal/utils"
 )
 
 type TaskStatusService interface {
-	Add(ctx context.Context, projectStatus domain.TaskStatus) (domain.TaskStatus, error)
-	Update(ctx context.Context, projectStatus domain.TaskStatus) (domain.TaskStatus, error)
-	Delete(ctx context.Context, id string) error
-	Get(ctx context.Context, id string) (domain.TaskStatus, error)
+	Add(ctx context.Context, taskStatus domain.TaskStatus) (domain.TaskStatus, error)
+	Update(ctx context.Context, taskStatus domain.TaskStatus) (domain.TaskStatus, error)
+	Delete(ctx context.Context, taskStatusID string) error
+	Get(ctx context.Context, taskStatusID string) (domain.TaskStatus, error)
 	Search(ctx context.Context, pager browser.Params, order browser.Order, filter domain.SearchTaskStatusesFilter) ([]domain.TaskStatus, browser.Result, error)
 }
 
 type taskStatusService struct {
-	database   database.Database
-	repository taskstatusrepository.TaskStatusRepository
+	db                   database.Database
+	authorizationService authorizationservice.AuthorizationService
+	taskStatusRepository taskstatusrepository.TaskStatusRepository
 }
 
-func NewService(db database.Database, repository taskstatusrepository.TaskStatusRepository) TaskStatusService {
-	return &taskStatusService{database: db, repository: repository}
+func NewService(db database.Database, authorizationService authorizationservice.AuthorizationService, taskStatusRepository taskstatusrepository.TaskStatusRepository) TaskStatusService {
+	return &taskStatusService{db: db, authorizationService: authorizationService, taskStatusRepository: taskStatusRepository}
+}
+
+func (service *taskStatusService) withUserAdminPermission(ctx context.Context, action func(currentUserID string) error) error {
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return fmt.Errorf("user not found in context")
+	}
+
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		return err
+	}
+
+	return action(currentContextUserID)
 }
 
 func (service *taskStatusService) Add(ctx context.Context, taskStatus domain.TaskStatus) (domain.TaskStatus, error) {
-	taskStatus.ID = utils.UUID()
-	err := service.repository.Add(ctx, taskStatus)
+	err := service.withUserAdminPermission(ctx, func(currentUserID string) error {
+		taskStatus.ID = utils.UUID()
+		err := service.taskStatusRepository.Add(ctx, service.db, taskStatus)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		return domain.TaskStatus{}, err
 	}
@@ -38,27 +60,56 @@ func (service *taskStatusService) Add(ctx context.Context, taskStatus domain.Tas
 }
 
 func (service *taskStatusService) Update(ctx context.Context, taskStatus domain.TaskStatus) (domain.TaskStatus, error) {
-	err := service.repository.Update(ctx, taskStatus)
+	err := service.withUserAdminPermission(ctx, func(currentUserID string) error {
+		err := service.taskStatusRepository.Update(ctx, service.db, taskStatus)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
 	if err != nil {
 		return domain.TaskStatus{}, err
 	}
 	return taskStatus, nil
 }
 
-func (service *taskStatusService) Delete(ctx context.Context, id string) error {
-	return service.repository.Delete(ctx, id)
+func (service *taskStatusService) Delete(ctx context.Context, taskStatusID string) error {
+	err := service.withUserAdminPermission(ctx, func(currentUserID string) error {
+		err := service.taskStatusRepository.Delete(ctx, service.db, taskStatusID)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
 }
 
-func (service *taskStatusService) Get(ctx context.Context, id string) (domain.TaskStatus, error) {
-	taskStatus, err := service.repository.Get(ctx, id)
+func (service *taskStatusService) Get(ctx context.Context, taskStatusID string) (domain.TaskStatus, error) {
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return domain.TaskStatus{}, fmt.Errorf("user not found in context")
+	}
+
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		return domain.TaskStatus{}, err
+	}
+	taskStatus, err := service.taskStatusRepository.Get(ctx, service.db, taskStatusID)
 	if err != nil {
-		return domain.TaskStatus{}, fmt.Errorf("[TaskStatusService] failed to get task status with ID %s: %w", id, err)
+		return domain.TaskStatus{}, fmt.Errorf("[TaskStatusService] failed to get task status with ID %s: %w", taskStatusID, err)
 	}
 	return taskStatus, nil
 }
 
 func (service *taskStatusService) Search(ctx context.Context, pager browser.Params, order browser.Order, filter domain.SearchTaskStatusesFilter) ([]domain.TaskStatus, browser.Result, error) {
-	taskStatuses, pagerResult, err := service.repository.Search(ctx, pager, order, filter)
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return nil, browser.Result{}, fmt.Errorf("user not found in context")
+	}
+
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		return nil, browser.Result{}, err
+	}
+	taskStatuses, pagerResult, err := service.taskStatusRepository.Search(ctx, service.db, pager, order, filter)
 	if err != nil {
 		return nil, browser.Result{}, fmt.Errorf("[TaskStatusService] failed to search task statuses: %w", err)
 	}
