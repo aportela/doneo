11774fdcf841e6ce86f98a4c1397a3c259d51e21
alex <766f6964+github@gmail.com>
2026-06-16 23:42:58 +0200

    old code removed

--- golang-backend/internal/middlewares/context_helpers.go
+++ golang-backend/internal/middlewares/context_helpers.go
@@ -23,17 +23,3 @@ func GetContextUser(ctx context.Context) (ContextUser, bool) {
 func SetContextUser(ctx context.Context, user ContextUser) context.Context {
 	return context.WithValue(ctx, contextUserKey, user)
 }
-
-/*
-const userIDKey contextKey = "userID"
-
-func GetUserIDFromContext(ctx context.Context) (string, bool) {
-	userID, ok := ctx.Value(userIDKey).(string)
-	return userID, ok
-}
-
-func SetUserIDIntoContext(ctx context.Context, userID string) context.Context {
-	return context.WithValue(ctx, userIDKey, userID)
-}
-
-*/
