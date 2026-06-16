e5e16986101648604db8d2d5a09a60c821b7996f
alex <766f6964+github@gmail.com>
2026-06-16 09:43:38 +0200

    added authorization checks

--- golang-backend/internal/services/taskservice/task.go
+++ golang-backend/internal/services/taskservice/task.go
@@ -2,6 +2,7 @@ package taskservice
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"time"
 
@@ -11,165 +12,172 @@ import (
 	"github.com/aportela/doneo/internal/middlewares"
 	"github.com/aportela/doneo/internal/repositories/tagrepository"
 	"github.com/aportela/doneo/internal/repositories/taskrepository"
+	"github.com/aportela/doneo/internal/services/authorizationservice"
 	"github.com/aportela/doneo/internal/services/historyoperationservice"
 	"github.com/aportela/doneo/internal/utils"
 )
 
 type TaskService interface {
-	Add(ctx context.Context, projectId string, task domain.Task) (domain.Task, error)
-	Update(ctx context.Context, projectId string, task domain.Task) (domain.Task, error)
-	Delete(ctx context.Context, projectId string, taskId string) error
-	Get(ctx context.Context, taskId string) (domain.Task, error)
+	Add(ctx context.Context, projectID string, task domain.Task) (domain.Task, error)
+	Update(ctx context.Context, projectID string, task domain.Task) (domain.Task, error)
+	Delete(ctx context.Context, projectID string, taskID string) error
+	Get(ctx context.Context, projectID string, taskID string) (domain.Task, error)
 	Search(ctx context.Context, pager browser.Params, order browser.Order, filter domain.SearchTaskFilter) ([]domain.Task, browser.Result, error)
 }
 
 type taskService struct {
-	database   database.Database
-	history    historyoperationservice.HistoryOperationService
-	repository taskrepository.TaskRepository
+	db                      database.Database
+	authorizationService    authorizationservice.AuthorizationService
+	historyOperationService historyoperationservice.HistoryOperationService
+	taskRepository          taskrepository.TaskRepository
+	tagRepository           tagrepository.TagRepository
 }
 
-func NewService(db database.Database, history historyoperationservice.HistoryOperationService, repository taskrepository.TaskRepository) TaskService {
-	return &taskService{database: db, history: history, repository: repository}
+func NewService(db database.Database, authorizationService authorizationservice.AuthorizationService, historyOperationService historyoperationservice.HistoryOperationService, taskRepository taskrepository.TaskRepository, tagRepository tagrepository.TagRepository) TaskService {
+	return &taskService{db: db, authorizationService: authorizationService, historyOperationService: historyOperationService, taskRepository: taskRepository, tagRepository: tagRepository}
 }
 
-func (service *taskService) Add(ctx context.Context, projectId string, task domain.Task) (domain.Task, error) {
-	tx, err := service.database.Begin()
-	if err != nil {
-		return domain.Task{}, err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
+func (service *taskService) Add(ctx context.Context, projectID string, task domain.Task) (domain.Task, error) {
+	err := service.authorizationService.WithTaskAddPermission(ctx, projectID, func(currentUserID string) error {
+		task.ID = utils.UUID()
+		task.CreatedBy.ID = currentUserID
+		task.CreatedAt = time.Now()
+		newTaskIndex, err := service.taskRepository.GetNextTaskIndex(ctx, service.db, projectID)
+		if err != nil {
+			return err
 		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
-	if !ok {
-		return domain.Task{}, fmt.Errorf("[TaskService] user ID not found in context")
-	}
-	task.ID = utils.UUID()
-	task.CreatedBy.ID = currentUserId
-	task.CreatedAt = time.Now()
-	task.Index, err = service.repository.GetNextTaskIndex(ctx, projectId)
-	if err != nil {
-		return domain.Task{}, err
-	}
-	err = service.repository.Add(ctx, projectId, task)
-	if err != nil {
-		return domain.Task{}, err
-	}
-	if len(task.Tags) > 0 {
-		tagRepository := tagrepository.NewRepository(service.database)
-		for _, tag := range task.Tags {
-			err = tagRepository.AddTaskTag(ctx, task.ID, tag)
-			if err != nil {
-				return domain.Task{}, err
+		task.Index = newTaskIndex
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.taskRepository.Add(ctx, tx, projectID, task); err != nil {
+				return err
 			}
-		}
-	}
-	_, err = service.history.AddTaskHistoryOperation(ctx, tx, projectId, task.ID, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: task.CreatedAt, OperationType: domain.EventTaskCreated})
-	if err != nil {
-		return domain.Task{}, err
-	}
-	err = tx.Commit()
+			if len(task.Tags) > 0 {
+				for _, taskTag := range task.Tags {
+					if err := service.tagRepository.AddTaskTag(ctx, tx, task.ID, taskTag); err != nil {
+						return err
+					}
+				}
+			}
+			if _, err := service.historyOperationService.AddTaskHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				task.ID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     task.CreatedAt,
+					OperationType: domain.EventTaskCreated,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
 	if err != nil {
 		return domain.Task{}, err
 	}
 	return task, nil
 }
 
-func (service *taskService) Update(ctx context.Context, projectId string, task domain.Task) (domain.Task, error) {
-	tx, err := service.database.Begin()
-	if err != nil {
-		return domain.Task{}, err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
-		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
-	if !ok {
-		return domain.Task{}, fmt.Errorf("[TaskService] user ID not found in context")
-	}
-	task.UpdatedAt = utils.CurrentTimePtr()
-	err = service.repository.Update(ctx, task)
-	if err != nil {
-		return domain.Task{}, err
-	}
-	tagRepository := tagrepository.NewRepository(service.database)
-	err = tagRepository.DeleteTaskTags(ctx, task.ID)
-	if err != nil {
-		return domain.Task{}, err
-	}
-	if len(task.Tags) > 0 {
-		for _, tag := range task.Tags {
-			err = tagRepository.AddTaskTag(ctx, task.ID, tag)
-			if err != nil {
-				return domain.Task{}, err
+func (service *taskService) Update(ctx context.Context, projectID string, task domain.Task) (domain.Task, error) {
+	err := service.authorizationService.WithTaskAddPermission(ctx, projectID, func(currentUserID string) error {
+		task.UpdatedAt = utils.CurrentTimePtr()
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.taskRepository.Update(ctx, tx, task); err != nil {
+				return err
 			}
-		}
-	}
-	_, err = service.history.AddTaskHistoryOperation(ctx, tx, projectId, task.ID, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: *task.UpdatedAt, OperationType: domain.EventTaskUpdated})
-	if err != nil {
-		return domain.Task{}, err
-	}
-	err = tx.Commit()
+			if err := service.tagRepository.DeleteTaskTags(ctx, tx, task.ID); err != nil {
+				return err
+			}
+			if len(task.Tags) > 0 {
+				for _, taskTag := range task.Tags {
+					if err := service.tagRepository.AddTaskTag(ctx, tx, task.ID, taskTag); err != nil {
+						return err
+					}
+				}
+			}
+			if _, err := service.historyOperationService.AddTaskHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				task.ID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     *task.UpdatedAt,
+					OperationType: domain.EventTaskUpdated,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
 	if err != nil {
 		return domain.Task{}, err
 	}
 	return task, nil
 }
 
-func (service *taskService) Delete(ctx context.Context, projectId string, taskId string) error {
-	tx, err := service.database.Begin()
-	if err != nil {
-		return err
-	}
-	defer func() {
-		if p := recover(); p != nil {
-			_ = tx.Rollback()
-			panic(p)
-		} else if err != nil {
-			_ = tx.Rollback()
-		}
-	}()
-	currentUserId, ok := middlewares.GetUserIDFromContext(ctx)
-	if !ok {
-		return fmt.Errorf("[TaskService] user ID not found in context")
-	}
-	err = service.repository.Delete(ctx, taskId, time.Now().UnixMilli())
-	if err != nil {
-		return err
-	}
-	_, err = service.history.AddTaskHistoryOperation(ctx, tx, projectId, taskId, domain.HistoryOperation{ID: utils.UUID(), CreatedBy: domain.UserBase{ID: currentUserId}, CreatedAt: time.Now(), OperationType: domain.EventTaskDeleted})
-	if err != nil {
-		return err
-	}
-	return tx.Commit()
+func (service *taskService) Delete(ctx context.Context, projectID string, taskID string) error {
+	err := service.authorizationService.WithTaskAddPermission(ctx, projectID, func(currentUserID string) error {
+		deletedAt := time.Now()
+		return database.WithTx(ctx, service.db, func(tx *sql.Tx) error {
+			if err := service.taskRepository.Delete(ctx, tx, taskID, deletedAt.UnixMilli()); err != nil {
+				return err
+			}
+			if _, err := service.historyOperationService.AddTaskHistoryOperation(
+				ctx,
+				tx,
+				projectID,
+				taskID,
+				domain.HistoryOperation{
+					ID:            utils.UUID(),
+					CreatedBy:     domain.UserBase{ID: currentUserID},
+					CreatedAt:     deletedAt,
+					OperationType: domain.EventTaskUpdated,
+				},
+			); err != nil {
+				return err
+			}
+			return nil
+		})
+	})
+	return err
 }
 
-func (service *taskService) Get(ctx context.Context, taskId string) (domain.Task, error) {
-	task, err := service.repository.Get(ctx, taskId)
-	if err != nil {
-		return domain.Task{}, fmt.Errorf("[TaskService] failed to get task with ID %s: %w", taskId, err)
+func (service *taskService) Get(ctx context.Context, projectID string, taskID string) (domain.Task, error) {
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return domain.Task{}, fmt.Errorf("user not found in context")
 	}
-	tags, err := tagrepository.NewRepository(service.database).GetTaskTags(ctx, task.ID)
-	if err != nil {
-		return domain.Task{}, fmt.Errorf("[TaskService] failed to get task with ID %s: %w", taskId, err)
+	if err := service.authorizationService.RequireTaskViewPermission(ctx, currentContextUserID, projectID); err != nil {
+		return domain.Task{}, err
 	}
-	task.Tags = tags
+	task, err := service.taskRepository.Get(ctx, service.db, taskID)
+	if err != nil {
+		return domain.Task{}, fmt.Errorf("[TaskService] failed to get task with ID %s: %w", taskID, err)
+	}
+	taskTags, err := service.tagRepository.GetTaskTags(ctx, service.db, task.ID)
+	if err != nil {
+		return domain.Task{}, fmt.Errorf("[TaskService] failed to get task with ID %s: %w", taskID, err)
+	}
+	task.Tags = taskTags
 	return task, nil
 }
 
 func (service *taskService) Search(ctx context.Context, pager browser.Params, order browser.Order, filter domain.SearchTaskFilter) ([]domain.Task, browser.Result, error) {
-	tasks, pagerResult, err := service.repository.Search(ctx, pager, order, filter)
+	currentContextUserID, ok := middlewares.GetUserIDFromContext(ctx)
+	if !ok {
+		return nil, browser.Result{}, fmt.Errorf("user not found in context")
+	}
+	if err := service.authorizationService.RequireUserAdminPermission(ctx, currentContextUserID); err != nil {
+		// filter by tasks visible by current user when admin flag is not set
+		filter.ViewByUserId = &currentContextUserID
+	}
+	tasks, pagerResult, err := service.taskRepository.Search(ctx, service.db, pager, order, filter)
 	if err != nil {
 		return nil, browser.Result{}, fmt.Errorf("[TaskService] failed to search tasks: %w", err)
 	}
