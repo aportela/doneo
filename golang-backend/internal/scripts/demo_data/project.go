ed2ea28aed81bd86980f07872df49105c8032c03
alex <766f6964+github@gmail.com>
2026-06-16 12:07:23 +0200

    code updated to last service changes

--- golang-backend/internal/scripts/demo_data/project.go
+++ golang-backend/internal/scripts/demo_data/project.go
@@ -231,9 +231,9 @@ func randomText(n int) string {
 
 func createProjects(db database.Database, projectTypeIds []string, projectPriorityIds []string, projectStatusIds []string, userIds []string, roleIds []string, taskStatusIds []string, taskPriorityIds []string, count int) []string {
 	var newProjectIds []string
-	historyOperationService := historyoperationservice.NewService(historyoperationrepository.NewRepository())
 	projectPermissionRepository := projectpermissionrepository.NewRepository()
 	authorizationService := authorizationservice.NewService(db, cache.NewPermissionCache(), userrepository.NewRepository(), projectPermissionRepository)
+	historyOperationService := historyoperationservice.NewService(authorizationService, historyoperationrepository.NewRepository())
 	projectService := projectservice.NewService(db, authorizationService, historyOperationService, projectrepository.NewRepository())
 	noteService := noteservice.NewService(db, authorizationService, historyOperationService, noterepository.NewRepository())
 	projectPermissionService := projectpermissionservice.NewService(db, authorizationService, historyOperationService, projectPermissionRepository)
