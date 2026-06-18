1c6e0f502e1bb1e264372408d1cbe0db94b4b8f4
alex <766f6964+github@gmail.com>
2026-06-18 15:12:58 +0200

    added local storage key for saving custom datetime mask

--- vite-vue-ts-frontend/src/pages/ProfilePage.vue
+++ vite-vue-ts-frontend/src/pages/ProfilePage.vue
@@ -1,18 +1,28 @@
 <script setup lang="ts">
-    import { computed, ref } from 'vue';
+    import { computed, ref, watch } from 'vue';
 
     import dayjs from 'dayjs'
 
+
     import { NTabs, NTabPane, NCard, NAvatar, NFlex, NInputGroup, NInput, NButton, NButtonGroup, NPopover, NIcon, NGrid, NGridItem } from 'naive-ui';
     import { IconFileUpload, IconTrash, IconSun, IconMoon, IconInfoCircle } from '@tabler/icons-vue';
+
+    import { createStorageEntry } from "../shared/composables/localStorage";
 
     import { useColorSchemeStore } from '../stores/colorScheme';
 
     const colorSchemeStore = useColorSchemeStore();
 
+    const localStorageDatetimeFormatMask = createStorageEntry<string | null>("datetimeFormatMask", "YYYY-MM-DD HH:MM:SS");
+
     const currentTab = ref<string>("mySettings");
 
-    const currentDatetimeMask = ref<string>("YYYY-MM-DD HH:MM:SS");
+    const currentDatetimeMask = ref<string | null>(localStorageDatetimeFormatMask.get());
+
+    watch(() => currentDatetimeMask.value, (newValue) => {
+
+        localStorageDatetimeFormatMask.set(newValue || "YYYY-MM-DD HH:MM:SS")
+    });
 
     const currentDatetimeMaskPreview = computed<string | null>({
         get() {
--- vite-vue-ts-frontend/src/shared/types/idate.ts
+++ vite-vue-ts-frontend/src/shared/types/idate.ts
@@ -1,3 +1,16 @@
+import dayjs from "dayjs";
+
+import { createStorageEntry } from "../composables/localStorage";
+
+const localStorageDatetimeFormatMask = createStorageEntry<string | null>(
+  "datetimeFormatMask",
+  "YYYY-MM-DD HH:MM:SS",
+);
+
+// TODO: use store for real time changes without refresh page ???
+const currentDatetimeMask =
+  localStorageDatetimeFormatMask.get() ?? "YYYY-MM-DD HH:MM:SS";
+
 export class IDate {
   // TODO: readonly msTimestamp ?, remove nulls
   msTimestamp: number | null;
@@ -22,5 +35,12 @@ export class IDate {
       this.date = new Date(this.msTimestamp);
     }
     return this.date?.toLocaleString();
+  };
+
+  toCustomMaskString = () => {
+    if (this.date === null && this.msTimestamp !== null) {
+      this.date = new Date(this.msTimestamp);
+    }
+    return dayjs(this.date).format(currentDatetimeMask);
   };
 }
