b0f931bf159596e0d21015bec4477c141d80eb96
alex <766f6964+github@gmail.com>
2026-06-21 22:02:50 +0200

    new datetime composable

---
+++ vite-vue-ts-frontend/src/shared/composables/datetime.ts
@@ -0,0 +1,11 @@
+export const geti18nTimeParts = (seconds: number) => {
+  const days = Math.floor(seconds / 86400);
+  const hours = Math.floor((seconds % 86400) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+
+  return [
+    { key: "shared.labels.time.day", count: days },
+    { key: "shared.labels.time.hour", count: hours },
+    { key: "shared.labels.time.minute", count: minutes },
+  ].filter(({ count }) => count > 0);
+};
