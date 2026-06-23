3a19629c4ea1084087b053341c6d6a8d2b3fa2a6
alex <766f6964+github@gmail.com>
2026-06-23 11:42:26 +0200

    use composable menu in top menu

--- vite-vue-ts-frontend/src/layouts/TopMenu.vue
+++ vite-vue-ts-frontend/src/layouts/TopMenu.vue
@@ -1,189 +1,52 @@
 <script setup lang="ts">
-    import { NMenu, type MenuOption } from 'naive-ui'
-    import { IconUserCheck, IconBug, IconSitemap, IconFileAnalytics, IconSettings, IconUserCircle, IconPresentation, IconChartHistogram, IconUsers, IconBookmark, IconFlagBolt, IconAdjustmentsBolt, IconLogout, IconId } from '@tabler/icons-vue';
-    import { ref, watch, h } from 'vue'
-    import { useRouter } from "vue-router";
-    import { RouterLink } from 'vue-router'
-    import { renderIcon } from '../shared/composables/naive-ui-icon';
+    import { nextTick } from 'vue'
+    import { useRoute } from 'vue-router'
 
-    const router = useRouter();
-    const currentTab = ref<string | number>("home");
+    import { NMenu } from 'naive-ui'
 
-    const commonIconSize = 20;
+    import { menuOptionIconSize, useMenu } from '../shared/types/menu';
 
-    watch(() => currentTab.value, (newValue) => {
-        router.push(
-            { name: newValue.toString() }
-        ).catch((e) => {
-            console.error(e);
-        });
+    import { useColorSchemeStore } from '../stores/colorScheme';
+    import { useUserSettingsStore } from '../stores/userSettings';
+
+    defineProps({
+        collapsed: Boolean
     });
 
-    function handleUpdateValue(_key: string, _item: MenuOption) {
+
+    const route = useRoute();
+
+    const colorSchemeStore = useColorSchemeStore();
+    const userSettingsStore = useUserSettingsStore();
+
+    const { menuOptions, lightTheme, darkTheme, notificationsDisabled, notificationsEnabled } = useMenu();
+
+    const handleMenuSelect = (menuOptionKey: string) => {
+        switch (menuOptionKey) {
+            case "disableNotifications":
+            case "enableNotifications":
+                userSettingsStore.toggleNotifications();
+                nextTick(() => {
+                    notificationsEnabled.value = userSettingsStore.hasNotificationsEnabled;
+                    notificationsDisabled.value = !userSettingsStore.hasNotificationsEnabled;
+                });
+                break;
+            case "switchToLightTheme":
+            case "switchToDarkTheme":
+                colorSchemeStore.toggle();
+                nextTick(() => {
+                    lightTheme.value = colorSchemeStore.light;
+                    darkTheme.value = colorSchemeStore.dark;
+                });
+                break;
+        }
     }
-
-    const menuOptions: MenuOption[] = [
-        {
-            label: () =>
-                h(
-                    RouterLink,
-                    {
-                        to: {
-                            name: 'home',
-                            params: {
-                            }
-                        }
-                    },
-                    { default: () => 'Overview' }
-                ),
-            key: 'home',
-            icon: renderIcon(IconPresentation)(commonIconSize),
-        },
-        {
-            label: () =>
-                h(
-                    RouterLink,
-                    {
-                        to: {
-                            name: 'projects',
-                            params: {
-                            }
-                        }
-                    },
-                    { default: () => 'Projects' }
-                ),
-            key: 'projects',
-            icon: renderIcon(IconSitemap)(commonIconSize),
-        },
-        {
-            label: "Tasks",
-            key: 'tasks',
-            disabled: true,
-            icon: renderIcon(IconBug)(commonIconSize)
-        },
-        {
-            label: "Reports",
-            key: "reports",
-            disabled: true,
-            icon: renderIcon(IconFileAnalytics)(commonIconSize)
-        },
-        {
-            label: "Charts",
-            key: 'charts',
-            disabled: true,
-            icon: renderIcon(IconChartHistogram)(commonIconSize)
-        },
-        {
-            label: "Settings",
-            key: 'settings',
-            show: false,
-            icon: renderIcon(IconSettings)(commonIconSize),
-            children: [
-                {
-                    label: () =>
-                        h(
-                            RouterLink,
-                            {
-                                to: {
-                                    name: 'users',
-                                    params: {
-                                    }
-                                }
-                            },
-                            { default: () => 'Users' }
-                        ),
-                    key: 'users',
-                    icon: renderIcon(IconUsers)(commonIconSize)
-                },
-                {
-                    label: 'Roles',
-                    key: 'roles',
-                    disabled: true,
-                    icon: renderIcon(IconUserCheck)(commonIconSize),
-                },
-                {
-                    label: "Project",
-                    key: 'projectSettings',
-                    icon: renderIcon(IconSettings)(commonIconSize),
-                    children: [
-                        {
-                            label: () =>
-                                h(
-                                    RouterLink,
-                                    {
-                                        to: {
-                                            name: 'manageProjectTypes',
-                                            params: {
-                                            }
-                                        }
-                                    },
-                                    { default: () => 'Type' }
-                                ),
-                            key: "projectTypes",
-                            icon: renderIcon(IconBookmark)(commonIconSize)
-                        },
-                        {
-                            label: () =>
-                                h(
-                                    RouterLink,
-                                    {
-                                        to: {
-                                            name: 'manageProjectPriorities',
-                                            params: {
-                                            }
-                                        }
-                                    },
-                                    { default: () => 'Priority' }
-                                ),
-                            key: "projectPriorities",
-                            icon: renderIcon(IconFlagBolt)(commonIconSize)
-                        },
-                        {
-                            label: () =>
-                                h(
-                                    RouterLink,
-                                    {
-                                        to: {
-                                            name: 'manageProjectStatuses',
-                                            params: {
-                                            }
-                                        }
-                                    },
-                                    { default: () => 'Status' }
-                                ),
-                            key: "projectStatuses",
-                            icon: renderIcon(IconAdjustmentsBolt)(commonIconSize)
-
-                        }
-                    ]
-                },
-            ]
-        },
-        {
-            label: "John Doe",
-            key: 'myuser',
-            icon: renderIcon(IconUserCircle)(commonIconSize),
-            children: [
-                {
-                    label: "Profile",
-                    key: "profile",
-                    disabled: true,
-                    icon: renderIcon(IconId)(commonIconSize),
-                },
-                {
-                    label: "Logout",
-                    key: "signout",
-                    icon: renderIcon(IconLogout)(commonIconSize),
-                }
-            ]
-        },
-    ];
 </script>
 
 <template>
     <div class="header">
-        <n-menu :collapsed-width="64" :collapsed-icon-size="commonIconSize" :options="menuOptions" mode="horizontal"
-            @update:value="handleUpdateValue" style="width: max-content;" />
+        <n-menu mode="horizontal" :collapsed-width="64" :collapsed-icon-size="menuOptionIconSize" :options="menuOptions"
+            :value="route.name as string" accordion :collapsed="collapsed" @update:value="handleMenuSelect" />
     </div>
 </template>
 
