c8786e538aea5348d2b7ac27fa0243f726ddbc8c
alex <766f6964+github@gmail.com>
2026-07-02 21:52:23 +0200

    testing new markdown modal editor

---
+++ vite-vue-ts-frontend/src/shared/components/modals/MarkdownEditorModal.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import { NModal } from 'naive-ui';
+
+    import { MdEditor } from 'md-editor-v3';
+    import 'md-editor-v3/lib/style.css';
+
+    import { useColorSchemeStore } from '../../../stores/colorScheme';
+
+    const colorSchemeStore = useColorSchemeStore();
+
+    const show = defineModel<boolean>('show', { default: false });
+    const markDown = defineModel<string>('markDown');
+</script>
+
+<template>
+    <n-modal v-model:show="show" closable close-on-esc mask-closable preset="card" size="huge" bordered>
+        <MdEditor v-model="markDown" :theme="colorSchemeStore.dark ? 'dark' : 'light'" language="en-US" />
+    </n-modal>
+</template>
+
+<style lang="css" scoped></style>
