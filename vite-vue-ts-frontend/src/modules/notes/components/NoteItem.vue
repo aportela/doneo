86d8062d0611d6584e732d6f554f303125f9bca1
alex <766f6964+github@gmail.com>
2026-07-10 00:32:59 +0200

    use new markdown editor/preview

--- vite-vue-ts-frontend/src/modules/notes/components/NoteItem.vue
+++ vite-vue-ts-frontend/src/modules/notes/components/NoteItem.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-    import { ref, h, watch, computed, nextTick, onMounted } from "vue";
+    import { ref, h, watch, nextTick, computed } from "vue";
     import { useI18n } from "vue-i18n";
 
-    import { NCard, NFlex, NButtonGroup, NButton, NIcon, NFormItem, NInput, useDialog, type InputInst } from 'naive-ui';
-    import { IconDeviceFloppy, IconCancel, IconEdit, IconTrash } from '@tabler/icons-vue';
+    import { NCard, NFlex, NButtonGroup, NButton, NIcon, NFormItem, useDialog, NDropdown, type InputInst, type DropdownOption } from 'naive-ui';
+    import { IconDeviceFloppy, IconCancel, IconEdit, IconTrash, IconDots } from '@tabler/icons-vue';
 
     import { useUserSettingsStore } from '../../../stores/userSettings.ts';
     import { renderIcon } from "../../../shared/composables/naive-ui-icon.ts";
@@ -12,7 +12,7 @@
 
     import type { NoteItemMode } from "../types/item-mode.ts";
 
-    import { useMarkdown } from "../../../shared/composables/useMarkdown.ts";
+    import ToggleMarkDownEditor from "../../../shared/components/form-blocks/ToggleMarkDownEditor.vue";
 
     interface NoteItemProps {
         readOnly?: boolean;
@@ -25,14 +25,11 @@
 
     const { t } = useI18n();
     const dialog = useDialog();
-    const { render, toMarkdown } = useMarkdown();
     const userSettingsStore = useUserSettingsStore();
 
     const currentMode = ref<NoteItemMode>(!!props.note.id ? "view" : "add");
 
-    const body = ref<string>("");
-
-    const htmlMarkDownBodyPreview = computed(() => render(props.note.body));
+    const body = ref<string>(props.note.body);
 
     const onConfirmDelete = () => {
         dialog.warning({
@@ -84,146 +81,78 @@
         }
     });
 
-    const insertAtCursor = (value: string) => {
-        const el = document.activeElement as HTMLTextAreaElement
-        if (!el) {
-            body.value += value
-            return
+
+    const noteOpts = computed(() => [
+        {
+            label: t("shared.buttons.Update.label"),
+            key: "update",
+            icon: renderIcon(IconEdit)(18),
+        },
+        {
+            label: t("shared.buttons.Delete.label"),
+            key: "delete",
+            icon: renderIcon(IconTrash)(18),
         }
+    ]);
 
-        const start = el.selectionStart ?? body.value.length
-        const end = el.selectionEnd ?? body.value.length
-
-        body.value =
-            body.value.slice(0, start) +
-            value +
-            body.value.slice(end)
-
-        // restore cursor
-        requestAnimationFrame(() => {
-            el.selectionStart = el.selectionEnd = start + value.length
-        })
-    }
-
-    const pastedWithoutFormat = ref<boolean>(false);
-    const onKeyDown = (event: KeyboardEvent) => {
-        if (
-            event.ctrlKey &&
-            event.shiftKey &&
-            event.key.toLowerCase() === 'v') {
-            pastedWithoutFormat.value = true;
-        } else {
-            pastedWithoutFormat.value = false;
+    const onSelectNoteAction = (key: string | number, _option: DropdownOption) => {
+        switch (key) {
+            case "update":
+                onUpdate();
+                break;
+            case "delete":
+                onConfirmDelete();
+                break;
         }
     };
-
-    const onPaste = (e: ClipboardEvent) => {
-        const clipboard = e.clipboardData
-        if (!clipboard) return
-
-        const html = clipboard.getData('text/html')
-        const plain = clipboard.getData('text/plain')
-
-        let markdown = plain
-
-        if (html && !pastedWithoutFormat.value) {
-            markdown = toMarkdown(html)
-        }
-
-        e.preventDefault()
-
-        insertAtCursor(markdown)
-    };
-
-    onMounted(() => {
-        if (!props.note.id) {
-            nextTick(() => {
-                bodyRef.value?.focus();
-            });
-        }
-    });
-
 </script>
 
 <template>
     <n-card size="small" bordered>
         <div class="note-header">
-            <div class="note-user">
-                <AvatarUserName :user-id="props.note.createdBy.id" :user-name="props.note.createdBy.name" />
-            </div>
-            <span class="note-date">
-                <div>
-                    Created at: {{ props.note.createdAt?.toCustomMaskString(userSettingsStore.currentDatetimeMask) }}
+            <n-flex justify="space-between" align="center">
+                <AvatarUserName :user-id="props.note.createdBy.id" :user-name="props.note.createdBy.name">
+                    <template #after>
+                        <span>commented on: {{
+                            props.note.createdAt?.toCustomMaskString(userSettingsStore.currentDatetimeMask)
+                            }}</span>
+                        <span v-if="props.note.updatedAt">(comment updated on: {{
+                            props.note.updatedAt?.toCustomMaskString(userSettingsStore.currentDatetimeMask) }})
+                        </span>
+                    </template>
+                </AvatarUserName>
+                <div v-if="!props.readOnly">
+                    <n-dropdown :options="noteOpts" trigger="click" @select="onSelectNoteAction"
+                        v-if="currentMode === 'view'">
+                        <n-button size="tiny">
+                            <template #icon>
+                                <n-icon :component="IconDots" />
+                            </template>
+                        </n-button>
+                    </n-dropdown>
+                    <n-button-group size="small" class="doneo-note-bottom-action-buttons" v-else>
+                        <n-button @click="onSave" :disabled="!body">
+                            <template #icon>
+                                <n-icon :component="IconDeviceFloppy"></n-icon>
+                            </template>
+                            {{ t("shared.buttons.Save.label") }}
+                        </n-button>
+                        <n-button @click="onCancel" v-if="note.id">
+                            <template #icon>
+                                <n-icon :component="IconCancel"></n-icon>
+                            </template>
+                            {{ t("shared.buttons.Cancel.label") }}
+                        </n-button>
+                    </n-button-group>
                 </div>
-                <div v-if="props.note.updatedAt">
-                    Updated at: {{ props.note.updatedAt?.toCustomMaskString(userSettingsStore.currentDatetimeMask) }}
-                </div>
-            </span>
+            </n-flex>
         </div>
-
         <n-form-item>
-            <div v-if="currentMode === 'view'" v-html="htmlMarkDownBodyPreview" />
-            <n-input v-else placeholder="Type note body" v-model:value="body" type="textarea" rows="6" ref="bodyRef"
-                @paste="onPaste" @keydown="onKeyDown" />
+            <ToggleMarkDownEditor v-model:value="body" hide-preview placeholder="Add note"
+                :read-only="currentMode === 'view'" auto-focus />
         </n-form-item>
-
-        <n-flex justify="end">
-            <n-button-group size="small" class="doneo-note-bottom-action-buttons"
-                v-if="['add', 'update'].includes(currentMode)">
-                <n-button @click="onSave" :disabled="!body">
-                    <template #icon>
-                        <n-icon :component="IconDeviceFloppy"></n-icon>
-                    </template>
-                    {{ t("shared.buttons.Save.label") }}
-                </n-button>
-                <n-button @click="onCancel" v-if="note.id">
-                    <template #icon>
-                        <n-icon :component="IconCancel"></n-icon>
-                    </template>
-                    {{ t("shared.buttons.Cancel.label") }}
-                </n-button>
-            </n-button-group>
-            <n-button-group size="small" class="doneo-note-bottom-action-buttons" v-else-if="!props.readOnly">
-                <n-button @click="onUpdate">
-                    <template #icon>
-                        <n-icon :component="IconEdit"></n-icon>
-                    </template>
-                    {{ t("shared.buttons.Update.label") }}
-                </n-button>
-                <n-button @click="onConfirmDelete">
-                    <template #icon>
-                        <n-icon :component="IconTrash"></n-icon>
-                    </template>
-                    {{ t("shared.buttons.Delete.label") }}
-                </n-button>
-            </n-button-group>
-        </n-flex>
     </n-card>
 </template>
 
 
-<style lang="css" scoped>
-    .note-header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 12px;
-    }
-
-    .note-user {
-        display: flex;
-        align-items: center;
-        gap: 8px;
-        font-weight: 500;
-    }
-
-    .note-date {
-        font-size: 12px;
-        color: #999;
-    }
-
-    .doneo-note-bottom-action-buttons {
-        margin-top: 16px;
-    }
-
-</style>
+<style lang="css" scoped></style>
