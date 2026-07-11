5b84fd25910cf9f4b6b4a6be304284a761dd6cce
alex <766f6964+github@gmail.com>
2026-07-11 12:36:04 +0200

    null values removed

--- vite-vue-ts-frontend/src/modules/users/models/user.ts
+++ vite-vue-ts-frontend/src/modules/users/models/user.ts
@@ -9,54 +9,56 @@ interface UserPermissions {
 }
 
 export class UserBase {
-  id: string | null;
-  name: string | null;
+  id: string;
+  name: string;
 
   constructor(data?: UserBaseDTO) {
-    this.id = data?.id ?? null;
-    this.name = data?.name ?? null;
+    this.id = data?.id ?? "";
+    this.name = data?.name ?? "";
   }
 
   toDTO(): UserBaseDTO {
     return {
-      id: this.id ?? "",
-      name: this.name ?? "",
+      id: this.id,
+      name: this.name,
     };
   }
 }
 
 export class User {
-  id: string | null;
-  name: string | null;
-  email: string | null;
-  password: string | null;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
   permissions: UserPermissions;
-  createdAt: IDate | null;
+  createdAt: IDate;
   updatedAt: IDate | null;
   deletedAt: IDate | null;
 
   constructor(data?: UserDTO) {
-    this.id = data?.id ?? null;
-    this.name = data?.name ?? null;
-    this.email = data?.email ?? null;
-    this.password = null;
+    this.id = data?.id ?? "";
+    this.name = data?.name ?? "";
+    this.email = data?.email ?? "";
+    this.password = "";
     this.permissions = {
       isSuperUser: data?.permissions.isSuperUser ?? false,
     };
-    this.createdAt = data?.createdAt ? new IDate(data.createdAt) : null;
+    this.createdAt = data?.createdAt
+      ? new IDate(data.createdAt)
+      : new IDate(new Date().getTime());
     this.updatedAt = data?.updatedAt ? new IDate(data.updatedAt) : null;
     this.deletedAt = data?.deletedAt ? new IDate(data.deletedAt) : null;
   }
 
   toDTO(): UserDTO {
     return {
-      id: this.id ?? "",
-      name: this.name ?? "",
-      email: this.email ?? "",
+      id: this.id,
+      name: this.name,
+      email: this.email,
       permissions: {
-        isSuperUser: this.permissions?.isSuperUser ?? false,
+        isSuperUser: this.permissions.isSuperUser ?? false,
       },
-      createdAt: this.createdAt?.msTimestamp ?? 0,
+      createdAt: this.createdAt.msTimestamp ?? new Date().getTime(),
       updatedAt: this.updatedAt?.msTimestamp ?? null,
       deletedAt: this.deletedAt?.msTimestamp ?? null,
     };
@@ -65,5 +67,4 @@ export class User {
 
 export const MAX_NAME_LENGTH = 32;
 export const MAX_EMAIL_LENGTH = 255;
-
 export const MIN_PASSWORD_LENGTH = 4;
