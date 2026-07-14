e972ffe82caf2b318e53a99884dffd37f535d78d
alex <766f6964+github@gmail.com>
2026-07-14 22:24:13 +0200

    old header removed

--- vite-vue-ts-frontend/src/layouts/MainLayout.vue
+++ vite-vue-ts-frontend/src/layouts/MainLayout.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
     import { ref, onMounted, onBeforeUnmount } from 'vue';
 
-    import { NLayout, NLayoutHeader, NLayoutSider, NLayoutContent, NSpin, NDialogProvider, NButton, NDrawer, NDrawerContent } from 'naive-ui'
+    import { NLayout, NLayoutHeader, NLayoutSider, NLayoutContent, NSpin, NDialogProvider, NButton, NDrawer, NDrawerContent, NIcon } from 'naive-ui'
 
     import { useBreakpoints } from '@vueuse/core';
     import { TokenManager } from '../modules/auth/services/tokenManager';
     import { useUserSettingsStore } from '../stores/userSettings';
     import { useLoadingStore } from '../stores/loading';
     import { useSessionStore } from '../stores/session';
-    import TopHeader from './TopHeader.vue';
     import SearchModal from '../shared/components/modals/SearchModal.vue';
     import ReAuthModal from '../modules/auth/components/ReAuthModal.vue';
     import RemoteAPIAlertModal from '../shared/components/modals/RemoteAPIAlertModal.vue';
     import NavigationMenu from '../shared/components/menus/NavigationMenu.vue';
+    import Doneo from '../shared/components/icons/Doneo.vue';
+    import { Search } from '@lucide/vue';
+    import NavigationBreadcrumb from '../shared/components/breadcrumbs/NavigationBreadcrumb.vue';
 
     const sessionStore = useSessionStore();
 
@@ -96,12 +98,22 @@
                     <n-layout-sider v-if="!isMobile" collapse-mode="width" :collapsed-width="62" :width="220"
                         :collapsed="isCollapsed" @collapse="isCollapsed = true" @expand="isCollapsed = false"
                         show-trigger="arrow-circle" bordered>
+                        <div class="brand">
+                            <n-icon class="brand-icon" :component="Doneo" :size="isCollapsed ? 24 : 16" />
+                            <span class="brand-name">Doneo</span>
+                        </div>
+                        <div style="margin: 8px 0px;">
+                            <span class="shortcut" style="margin-left: 24px;" @click="showSearchModal = true;">
+                                <n-icon :size="16" :component="Search" />
+                                Search... <kbd>Ctrl</kbd>+<kbd>K</kbd>
+                            </span>
+                        </div>
                         <NavigationMenu mode="vertical" :collapsed="isCollapsed" />
                     </n-layout-sider>
                     <n-layout>
                         <n-layout-header bordered>
-                            <TopHeader @open-search-modal="showSearchModal = true;" />
                             <n-button v-if="isMobile" quaternary circle @click="mobileMenuOpen = true">☰</n-button>
+                            <NavigationBreadcrumb />
                         </n-layout-header>
                         <n-layout-content>
                             <router-view />
@@ -118,9 +130,48 @@
 </template>
 
 <style lang="css" scoped>
+
+    /*
     .n-layout-content {
         height: calc(100vh - 64px);
         overflow: auto;
         padding: 16px;
     }
+*/
+    .brand {
+        display: flex;
+        align-items: center;
+        border-bottom: solid 1px var(--n-border-color);
+        padding: 7px 0px;
+    }
+
+    .brand-icon {
+        margin-left: 34px;
+    }
+
+    .brand span {
+        margin-left: 12px;
+    }
+
+    .shortcut {
+        display: flex;
+        align-items: center;
+        gap: 6px;
+        font-size: 12px;
+        padding: 2px 6px;
+        border: 1px solid rgba(20, 20, 22, 0.274);
+        border-radius: 16px;
+        width: 172px;
+        cursor: pointer;
+    }
+
+    kbd {
+        padding: 2px 8px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+        font-family: monospace;
+        font-size: 12px;
+        color: var(--n-text-color);
+    }
+
 </style>
