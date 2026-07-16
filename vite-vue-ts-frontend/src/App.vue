40cde2d058473531438234c06395803fea9a2957
alex <766f6964+github@gmail.com>
2026-07-16 19:38:05 +0200

    testing theme ovewrrides

--- vite-vue-ts-frontend/src/App.vue
+++ vite-vue-ts-frontend/src/App.vue
@@ -5,10 +5,36 @@
   import { useColorSchemeStore } from './stores/colorScheme';
 
   const colorSchemeStore = useColorSchemeStore();
+
+  import { type GlobalThemeOverrides } from "naive-ui";
+
+  const draculaTheme: GlobalThemeOverrides = {
+    common: {
+      // colores globales
+      primaryColor: "#BD93F9",
+      primaryColorHover: "#CDA7FF",
+      primaryColorPressed: "#A77BF3",
+
+      bodyColor: "#282A36",
+      cardColor: "#44475A",
+      modalColor: "#44475A",
+
+      textColorBase: "#F8F8F2",
+      textColor1: "#F8F8F2",
+      textColor2: "#F8F8F2",
+      textColor3: "#6272A4",
+
+      borderColor: "#6272A4",
+
+      inputColor: "#44475A",
+      tableColor: "#282A36",
+    },
+  };
 </script>
 
 <template>
-  <n-config-provider :theme="colorSchemeStore.dark ? darkTheme : null">
+  <n-config-provider :theme="colorSchemeStore.dark ? darkTheme : undefined"
+    :theme-overrides="colorSchemeStore.dark ? undefined : undefined">
     <n-notification-provider>
       <router-view />
     </n-notification-provider>
