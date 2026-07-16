6a17a19b17547ab4a1778db78715503ecd8a3319
alex <766f6964+github@gmail.com>
2026-07-16 15:41:26 +0200

    new column definition

--- vite-vue-ts-frontend/src/modules/projects/components/ProjectsTable.vue
+++ vite-vue-ts-frontend/src/modules/projects/components/ProjectsTable.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-    import { ref, computed } from 'vue';
+    import { ref, reactive, computed } from 'vue';
     import { useI18n } from "vue-i18n";
 
     import { NEmpty, NTag, NButtonGroup, NButton, NIcon } from 'naive-ui';
@@ -77,57 +77,59 @@
         isFilteredByCreator.value
     );
 
-    const columns = computed<TableHeaderColumn[]>(() => [
-        {
+    const columnDefinitions = reactive<Record<string, TableHeaderColumn>>({
+        slug: {
             label: t("modules.project.components.ProjectsTable.header.columns.slug"),
             field: "slug",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredBySlug.value,
         },
-        {
+        type: {
             label: t("modules.project.components.ProjectsTable.header.columns.type"),
             field: "type",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredByType.value,
         },
-        {
+        priority: {
             label: t("modules.project.components.ProjectsTable.header.columns.priority"),
             field: "priority",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredByPriority.value,
         },
-        {
+        status: {
             label: t("modules.project.components.ProjectsTable.header.columns.status"),
             field: "status",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredByStatus.value,
         },
-        {
+        summary: {
             label: t("modules.project.components.ProjectsTable.header.columns.summary"),
             field: "summary",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredBySummary.value,
         },
-        {
+        createdAt: {
             label: t("modules.project.components.ProjectsTable.header.columns.createdAt"),
             field: "createdAt",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredByCreationDate.value,
         },
-        {
+        createdBy: {
             label: t("modules.project.components.ProjectsTable.header.columns.createdBy"),
             field: "createdBy",
             visible: true,
             sortable: true,
             isFiltered: () => isFilteredByCreator.value,
         },
-    ]);
+    });
+
+    const columns = computed<TableHeaderColumn[]>(() => Object.values(columnDefinitions))
 
     const onSort = (sort: Sort) => {
         emit("sort", sort);
@@ -163,44 +165,53 @@
         showDrawer.value = true;
         currentProject.value = project;
     };
+
+    const onShowColumn = (column: TableHeaderColumn) => {
+        columnDefinitions[column.field].visible = true;
+    };
+
+    const onHideColumn = (column: TableHeaderColumn) => {
+        columnDefinitions[column.field].visible = false;
+    };
 </script>
 
 <template>
     <ProjectResumeFloatingCard v-if="showDrawer && currentProject?.id" v-model:show="showDrawer"
         :project-id="currentProject?.id" />
-    <ManageTable size="small" :columns="columns" :current-sort="sort" @sort="onSort" @refresh="onRefresh" @add="onAdd">
+    <ManageTable size="small" :columns="columns" :current-sort="sort" @sort="onSort" @refresh="onRefresh" @add="onAdd"
+        @show-column="onShowColumn" @hide-column="onHideColumn">
         <template #thead>
             <tr>
-                <th>
+                <th v-if="columnDefinitions.slug.visible">
                     <TextFilterInput clearable :disabled="props.disabled" size="small"
                         :placeholder="t('modules.project.components.ProjectsTable.header.filters.slug.placeholder')"
                         v-model:value="filters.slug" @keydown-enter="onRefresh" />
                 </th>
-                <th>
+                <th v-if="columnDefinitions.type.visible">
                     <ProjectTypeSelector :disabled="props.disabled" size="small" v-model:id="filters.typeId"
                         :hide-prefix="true" clearable
                         :placeholder="t('modules.project.components.ProjectsTable.header.filters.type.placeholder')" />
                 </th>
-                <th>
+                <th v-if="columnDefinitions.priority.visible">
                     <ProjectPrioritySelector :disabled="props.disabled" size="small" v-model:id="filters.priorityId"
                         :hide-prefix="true" clearable
                         :placeholder="t('modules.project.components.ProjectsTable.header.filters.priority.placeholder')" />
                 </th>
-                <th>
+                <th v-if="columnDefinitions.status.visible">
                     <ProjectStatusSelector :disabled="props.disabled" size="small" v-model:id="filters.statusId"
                         :hide-prefix="true" clearable
                         :placeholder="t('modules.project.components.ProjectsTable.header.filters.status.placeholder')" />
                 </th>
-                <th>
+                <th v-if="columnDefinitions.summary.visible">
                     <TextFilterInput clearable :disabled="props.disabled" size="small"
                         :placeholder="t('modules.project.components.ProjectsTable.header.filters.summary.placeholder')"
                         v-model:value="filters.summary" @keydown-enter="onRefresh" />
                 </th>
-                <th>
+                <th v-if="columnDefinitions.createdAt.visible">
                     <DateFilterSelect clearable ref="createdAtFilterRef" size="small" :disabled="props.disabled"
                         v-model:range="filters.createdAt" />
                 </th>
-                <th>
+                <th v-if="columnDefinitions.createdBy.visible">
                     <UserSelector hideAvatar clearable :disabled="props.disabled" size="small"
                         v-model:id="filters.createdByUserId"
                         :placeholder="t('modules.project.components.ProjectsTable.header.filters.creator.placeholder')" />
@@ -213,41 +224,43 @@
         <template #tbody>
             <tr v-for="project, index in items" :key="project.id ?? index"
                 :class="{ 'tr-archived-project': project.archivedAt.hasValue() }">
-                <td>
+                <td v-if="columnDefinitions.slug.visible">
                     {{ project.slug }}
                 </td>
-                <td><n-tag :bordered="false" :color="getNaiveUITagColorProperty(project.type.hexColor ?? '#888888')">{{
-                    project.type.name }}</n-tag>
+                <td v-if="columnDefinitions.type.visible"><n-tag :bordered="false"
+                        :color="getNaiveUITagColorProperty(project.type.hexColor ?? '#888888')">{{
+                            project.type.name }}</n-tag>
                 </td>
-                <td><n-tag :bordered="false"
+                <td v-if="columnDefinitions.priority.visible"><n-tag :bordered="false"
                         :color="getNaiveUITagColorProperty(project.priority.hexColor ?? '#888888')">{{
                             project.priority.name
                         }}</n-tag></td>
-                <td><n-tag :bordered="false"
+                <td v-if="columnDefinitions.status.visible"><n-tag :bordered="false"
                         :color="getNaiveUITagColorProperty(project.status.hexColor ?? '#888888')">{{
                             project.status.name }}</n-tag></td>
-                <td>
+                <td v-if="columnDefinitions.summary.visible">
                     <router-link :to="{ name: 'projectTab', params: { projectId: project.id, tab: 'metadata' } }"
                         class="doneo-link-text-color-default">{{
                             project.summary
                         }}</router-link>
                 </td>
-                <td>{{ project.createdAt.toCustomMaskString(userSettingsStore.currentDatetimeMask) }}</td>
-                <td>
+                <td v-if="columnDefinitions.createdAt.visible">{{
+                    project.createdAt.toCustomMaskString(userSettingsStore.currentDatetimeMask) }}</td>
+                <td v-if="columnDefinitions.createdBy.visible">
                     <AvatarUserName :user-id="project.createdBy.id" :user-name="project.createdBy.name" />
                 </td>
                 <td class="doneo-text-center">
                     <!-- TODO: use ManageTableActionButtons -->
-                    <n-button-group :size="DEFAULT_BUTTON_SIZE">
-                        <n-button :disabled="props.disabled" :size="DEFAULT_BUTTON_SIZE"
-                            @click="onShowProjectResume(project)">
+                    <n-button-group class="doneo-table-actions-button-group" :size="DEFAULT_BUTTON_SIZE">
+                        <n-button class="doneo-table-actions-button" :disabled="props.disabled"
+                            :size="DEFAULT_BUTTON_SIZE" @click="onShowProjectResume(project)">
                             {{ t("shared.buttons.Open.label") }}
                             <template #icon>
                                 <n-icon :size="22" :component="IconFilePencil" />
                             </template>
                         </n-button>
-                        <ChangeProjectStatusDropdown :disabled="props.disabled" :read-only="props.readOnly"
-                            :current-status="project.status"
+                        <ChangeProjectStatusDropdown className="doneo-table-actions-button" :disabled="props.disabled"
+                            :read-only="props.readOnly" :current-status="project.status"
                             @change="(status: ProjectStatus) => onStatusChange(project, status)" />
                     </n-button-group>
                 </td>
