76836910e02869ebb6267fd09faa380526bfdb40
alex <766f6964+github@gmail.com>
2026-07-17 15:41:37 +0200

    removed type

--- vite-vue-ts-frontend/src/shared/types/table-header-column.ts
+++ vite-vue-ts-frontend/src/shared/types/table-header-column.ts
@@ -1,13 +1,11 @@
 import type { VNodeChild } from "vue";
 
-type TableHeaderColumnAlign = "left" | "right" | "center";
-
 export interface TableHeaderColumn<T> {
   label: string;
   field: string;
   visible: boolean;
   sortable?: boolean;
-  align?: TableHeaderColumnAlign;
+  align?: "left" | "right" | "center";
   isFiltered?: () => boolean;
   render: (value: T) => VNodeChild;
 }
