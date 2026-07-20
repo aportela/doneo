3d954608c99df14234dccf7d01d64e8fe8561dfb
alex <766f6964+github@gmail.com>
2026-07-20 21:32:11 +0200

    added param type

--- vite-vue-ts-frontend/src/shared/composables/naive-ui-icon.ts
+++ vite-vue-ts-frontend/src/shared/composables/naive-ui-icon.ts
@@ -3,7 +3,7 @@ import { h, type Component } from "vue";
 import { NIcon } from "naive-ui";
 
 export const renderIcon = (icon: Component) => {
-  return (size = 32) =>
+  return (size: number = 32) =>
     () =>
       h(
         NIcon,
