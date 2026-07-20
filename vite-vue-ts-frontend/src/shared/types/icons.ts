813a44d28bd16fee09159f045601fd0e7a881208
alex <766f6964+github@gmail.com>
2026-07-20 21:32:02 +0200

    icon aliases file added

---
+++ vite-vue-ts-frontend/src/shared/types/icons.ts
@@ -0,0 +1,7 @@
+export {
+  User as DONEO_ICON_USER,
+  ShieldUser as DONEO_ICON_ADMIN_USER,
+  Trash2 as DONEO_ICON_ACTION_DELETE,
+  ArchiveRestore as DONEO_ICON_ACTIONRESTORE,
+  SquarePen as DONEO_ICON_ACTION_EDIT,
+} from "@lucide/vue";
