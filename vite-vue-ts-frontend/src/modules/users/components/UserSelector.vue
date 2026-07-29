31fd3854940c1e9b94bf91613e8cd16185ad2f81
alex <766f6964+github@gmail.com>
2026-07-29 14:17:47 +0200

    tabler icons replaced by lucide, code cleanups

--- vite-vue-ts-frontend/src/modules/users/components/UserSelector.vue
+++ vite-vue-ts-frontend/src/modules/users/components/UserSelector.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
     import { ref, shallowRef, reactive, computed, onMounted, onBeforeUnmount, nextTick } from 'vue';
 
-    import { NInputGroup, NButton, NSelect, NIcon, NAvatar, type SelectOption, type SelectSize, type SelectInst } from 'naive-ui';
-    import { IconAlertCircle, IconUserCircle } from '@tabler/icons-vue';
+    import { NInputGroup, NButton, NSelect, NIcon, NAvatar, type SelectOption, type SelectInst } from 'naive-ui';
 
     import { useCacheStore } from '../../../stores/cache';
     import { type AjaxStateInterface, defaultAjaxState, defaultAjaxStateRunning } from '../../../shared/types/ajaxState';
@@ -11,14 +10,14 @@
     import { appBus } from '../../../shared/composables/bus';
     import { handleAPIError } from '../../../api/client/errorHandler';
 
+    import { DONEO_ICON_ALERT, DONEO_ICON_CIRCLE_USER } from '../../../shared/types/icons';
+
     interface Props {
-        autoFocus?: boolean;
-        required?: boolean;
-        placeholder?: string;
         clearable?: boolean;
-        size?: SelectSize;
-        hideAvatar?: boolean;
         disabled?: boolean;
+        placeholder?: string;
+        autoFocus?: boolean;
+        hideAvatar?: boolean;
     }
 
     const props = defineProps<Props>();
@@ -33,23 +32,25 @@
 
     const userId = defineModel<string | null>('id');
 
-    const avatarURL = computed(() => userId.value ? `/api/wc/avatars/user/${userId.value}` : null);
+    const avatarURL = computed<string | null>(() => userId.value ? `/api/wc/avatars/user/${userId.value}` : null);
 
     const options = shallowRef<SelectOption[]>([]);
+
+    const focus = async () => {
+        await nextTick();
+        selectInstRef.value?.focus();
+    };
+
+    defineExpose({ focus });
 
     const onRefresh = async () => {
         Object.assign(state, defaultAjaxStateRunning);
         try {
-            if (cacheStore.users.length > 0) {
-                options.value = cacheStore.users.map((user: UserBaseResponse) => ({ label: user.name, value: user.id }));
-            } else {
+            if (cacheStore.users.length === 0) {
                 const response = await userService.searchBase();
-                options.value = response.users.map((user: UserBaseResponse) => ({ label: user.name, value: user.id }));
-                cacheStore.setUsersCache(response.users)
+                cacheStore.setUsersCache(response.users);
             }
-            if (props.autoFocus) {
-                focus();
-            }
+            options.value = cacheStore.users.map((user: UserBaseResponse) => ({ label: user.name, value: user.id }));
         } catch (error: unknown) {
             options.value.length = 0;
             state.ajaxErrors = true;
@@ -71,20 +72,11 @@
         }
         finally {
             state.ajaxRunning = false;
+            if (!state.ajaxErrors && props.autoFocus) {
+                focus();
+            }
         }
     };
-
-    const focus = () => {
-        nextTick(() => {
-            selectInstRef.value?.focus();
-        });
-    };
-
-    const reset = () => {
-        userId.value = null;
-    }
-
-    defineExpose({ reset });
 
     let stopBusReauthListener: () => void;
 
@@ -106,20 +98,17 @@
     <n-input-group>
         <div v-if="!props.hideAvatar">
             <n-avatar v-if="avatarURL" :src="avatarURL" color="transparent" />
-            <n-button secondary :disabled="true" class="doneo-cursor-default doneo-disable-opacity" v-else>
+            <n-button disabled class="doneo-cursor-default doneo-disable-opacity" v-else>
                 <template #icon>
-                    <n-icon :component="IconUserCircle">
-                    </n-icon>
+                    <n-icon :component="DONEO_ICON_CIRCLE_USER" />
                 </template>
             </n-button>
         </div>
-        <n-select filterable ref="selectInstRef" :required="props.required" auto :clearable="props.clearable"
-            v-model:value="userId" :options="options" :placeholder="props.placeholder" :size="props.size"
-            :disabled="isDisabled" />
-        <n-button secondary :disabled="true" class="doneo-cursor-default doneo-disable-opacity" v-if="state.ajaxErrors">
+        <n-select filterable ref="selectInstRef" auto :clearable="props.clearable" v-model:value="userId"
+            :options="options" :placeholder="props.placeholder" :disabled="isDisabled" />
+        <n-button disabled class="doneo-cursor-default doneo-disable-opacity" v-if="state.ajaxErrors">
             <template #icon>
-                <n-icon color="red" :component="IconAlertCircle">
-                </n-icon>
+                <n-icon color="red" :component="DONEO_ICON_ALERT" />
             </template>
         </n-button>
     </n-input-group>
