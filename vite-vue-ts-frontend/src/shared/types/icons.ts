2fc73cbc19ce63d86d111a67c0fce4e749701911
alex <766f6964+github@gmail.com>
2026-07-29 15:28:17 +0200

    new icon added

--- vite-vue-ts-frontend/src/shared/types/icons.ts
+++ vite-vue-ts-frontend/src/shared/types/icons.ts
@@ -1,5 +1,6 @@
 export {
   User as DONEO_ICON_USER,
+  UserKey as DONEO_ICON_ROLE,
   ShieldUser as DONEO_ICON_ADMIN_USER,
   Trash2 as DONEO_ICON_ACTION_DELETE,
   ArchiveRestore as DONEO_ICON_ACTIONRESTORE,
